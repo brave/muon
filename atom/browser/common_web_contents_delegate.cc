4c1e26ad48fcdeacd58026077312024570ddb457
bridiver <github@brianjohnson.cc>
2016-05-19 12:49:31 -0700

    fixes https://github.com/brave/browser-laptop/issues/1568

--- atom/browser/common_web_contents_delegate.cc
+++ atom/browser/common_web_contents_delegate.cc
@@ -247,13 +247,23 @@ content::WebContents* CommonWebContentsDelegate::OpenURLFromTab(
     content::WebContents* source,
     const content::OpenURLParams& params) {
   content::NavigationController::LoadURLParams load_url_params(params.url);
+  load_url_params.source_site_instance = params.source_site_instance;
   load_url_params.referrer = params.referrer;
+  load_url_params.frame_tree_node_id = params.frame_tree_node_id;
+  load_url_params.redirect_chain = params.redirect_chain;
   load_url_params.transition_type = params.transition;
   load_url_params.extra_headers = params.extra_headers;
   load_url_params.should_replace_current_entry =
-      params.should_replace_current_entry;
+    params.should_replace_current_entry;
   load_url_params.is_renderer_initiated = params.is_renderer_initiated;
-  load_url_params.should_clear_history_list = true;
+
+  // Only allows the browser-initiated navigation to use POST.
+  if (params.uses_post && !params.is_renderer_initiated) {
+    load_url_params.load_type =
+      content::NavigationController::LOAD_TYPE_BROWSER_INITIATED_HTTP_POST;
+    load_url_params.browser_initiated_post_data =
+      params.browser_initiated_post_data;
+  }
 
   source->GetController().LoadURLWithParams(load_url_params);
   return source;
