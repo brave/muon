9d8fe7acf8050531224a333b9a0399cddc0eadb0
Brian R. Bondy <netzen@gmail.com>
2016-05-25 09:37:00 -0400

    Update to Brave Node, native-mate and brightray
    Upgrade Node to v6.1.0
    Update native-mate for #4892
    Update Brightray w/ chomium 51.0.2704.57 dep
    Fix warning for VS2015
    update breakpad

--- script/lib/config.py
+++ script/lib/config.py
@@ -8,7 +8,7 @@ import sys
 
 BASE_URL = os.getenv('LIBCHROMIUMCONTENT_MIRROR') or \
     'https://s3.amazonaws.com/github-janky-artifacts/libchromiumcontent'
-LIBCHROMIUMCONTENT_COMMIT = '7ae12b2856b81f5bfa1290108f72c3e4b5c13c52'
+LIBCHROMIUMCONTENT_COMMIT = '538b5b5f96cf115318b577b9439d217f0a488bd0'
 
 PLATFORM = {
   'cygwin': 'win32',
