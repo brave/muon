43b0d422424873a902287da12b79bd2dbaf5a646
bridiver <github@brianjohnson.cc>
2016-05-31 23:23:14 -0700

    let the navigation controller manage history

--- atom/common/api/resources/ipc_bindings.js
+++ atom/common/api/resources/ipc_bindings.js
@@ -14,27 +14,6 @@ exports.didCreateDocumentElement = function() {
   window.confirm = function(message, title) {
     return ipc.sendSync('window-confirm', message, title);
   };
-
-  // copied from override.js
-  // TODO(bridiver) is this even necessary? I don't think we need
-  // or want these navigation controller hacks for nodeless renderers
-  var sendHistoryOperation = function() {
-    var args;
-    args = 1 <= arguments.length ? slice.call(arguments, 0) : [];
-    return ipc.send.apply(ipc, ['ELECTRON_NAVIGATION_CONTROLLER'].concat(slice.call(args)));
-  };
-
-  window.history.back = function() {
-    return sendHistoryOperation('goBack');
-  };
-
-  window.history.forward = function() {
-    return sendHistoryOperation('goForward');
-  };
-
-  window.history.go = function(offset) {
-    return sendHistoryOperation('goToOffset', offset);
-  };
 };
 
 exports.binding = ipc;
