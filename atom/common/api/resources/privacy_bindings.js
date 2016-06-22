4b57257ddd74dc38f6a79f2116754176d7ee1d83
bridiver <github@brianjohnson.cc>
2016-06-22 14:04:52 -0700

    fix extension support issues for LastPass

--- atom/common/api/resources/privacy_bindings.js
+++ atom/common/api/resources/privacy_bindings.js
@@ -2,12 +2,14 @@ var services = {
   passwordSavingEnabled: {
     get: function(details, cb) {
       cb({level_of_control: "not_controllable"});
-    }
+    },
+    set: function() {}
   },
   autofillEnabled: {
     get: function(details, cb) {
       cb({level_of_control: "not_controllable"});
-    }
+    },
+    set: function() {}
   }
 }
 
