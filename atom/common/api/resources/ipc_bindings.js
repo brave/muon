6065ab7bf9cad81e87cdf2c03a5e8d7ac0a824a4
Anthony Tseng <darkdh@gmail.com>
2016-10-25 15:48:47 -0400

    Overrides window.open for background and extension page
    fix https://github.com/brave/browser-laptop/issues/4686
    
    @Auditors: @bridiver, @jonathansampson

--- atom/common/api/resources/ipc_bindings.js
+++ atom/common/api/resources/ipc_bindings.js
@@ -5,19 +5,31 @@ exports.didCreateDocumentElement = function() {
   if (!window)
     return
 
-  var slice = [].slice;
-
   window.alert = function(message, title) {
-    return ipc.send('window-alert', message, title);
+    return ipc.send('window-alert', message, title)
   }
 
   window.confirm = function(message, title) {
-    return ipc.sendSync('window-confirm', message, title);
+    return ipc.sendSync('window-confirm', message, title)
   }
 
   window.prompt = function(text, defaultText) {
-    return ipc.sendSync('window-prompt', text, defaultText);
+    return ipc.sendSync('window-prompt', text, defaultText)
   }
+
+  if (window.chrome && window.chrome.tabs && window.chrome.tabs.create)
+    window.open = function(url, name, specs, replace) {
+      if (name !== undefined) {
+        throw new Error('name of window.open is not supported yet')
+      }
+      if (specs !== undefined) {
+        throw new Error('specs of window.open is not supported yet')
+      }
+      if (replace !== undefined) {
+        throw new Error('replace of window.open is not supported yet')
+      }
+      return window.chrome.tabs.create({url: url})
+    }
 }
 
-exports.binding = ipc;
+exports.binding = ipc
