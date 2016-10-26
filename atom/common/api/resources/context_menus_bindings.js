7e68e3ec7942a8b37c03e12e4b1596ba64618308
Anthony Tseng <darkdh@gmail.com>
2016-10-26 18:59:14 -0400

    Add chrome.contextMenus.create and chrome.contextMenus.removeAll
    Auditors: @bridiver, @jonathansampson

--- atom/common/api/resources/context_menus_bindings.js
+++ atom/common/api/resources/context_menus_bindings.js
@@ -1,15 +1,49 @@
-var ipc = require('ipc_utils');
-var process = requireNative('process');
-var extensionId = process.GetExtensionId();
+var ipc = require('ipc_utils')
+var process = requireNative('process')
+var extensionId = process.GetExtensionId()
+
+var id = 0
+
+var createMenuItemId = () => {
+  return 'context-menus-' + extensionId + id++
+}
+
+var createResponseId = () => {
+  return extensionId + id++
+}
 
 var binding = {
-  removeAll: function () {
-    console.warn('chrome.contentMenus.removeAll is not supported yet')
+  onClicked: {
+    addListener: function (cb) {
+      ipc.on('chrome-context-menus-clicked', function(evt, info, tab) {
+        cb(info, tab)
+      })
+    },
+    removeListener: function (cb) {
+      // TODO
+      // ipc.off('chrome-context-menus-clicked', cb)
+    }
+  },
+  removeAll: function (cb) {
+    var responseId = createResponseId()
+    cb && ipc.once('chrome-context-menus-remove-all-response-' + responseId, function(evt) {
+      cb()
+    })
+    ipc.send('chrome-context-menus-remove-all', responseId, extensionId)
   },
   create: function (properties, cb) {
-    console.warn('chrome.contentMenus.create is not supported yet')
-    cb && cb()
+    var responseId = createResponseId()
+    var menuItemId = properties.id || createMenuItemId()
+    cb && ipc.once('chrome-context-menus-create-response-' + responseId, function(evt) {
+      cb()
+    })
+    properties.onclick && ipc.on('chrome-context-menus-clicked', (evt, info, tab) => {
+      if (info.menuItemId === menuItemId) {
+        properties.onclick(info, tab)
+      }
+    })
+    ipc.send('chrome-context-menus-create', responseId, extensionId, menuItemId, properties)
   }
 }
 
-exports.binding = binding;
+exports.binding = binding
