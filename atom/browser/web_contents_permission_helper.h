215e69455234a7a873577e989c577f5afba4145c
yan <yan@mit.edu>
2016-11-11 15:40:44 -0800

    Switch order of security_origin and user_gesture
    address review comment https://github.com/brave/electron/pull/93/files/9eae22d48726a8441506a41cb19740815d122414#r87654073

--- atom/browser/web_contents_permission_helper.h
+++ atom/browser/web_contents_permission_helper.h
@@ -46,8 +46,8 @@ class WebContentsPermissionHelper
   void RequestPermission(
       content::PermissionType permission,
       const base::Callback<void(bool)>& callback,
-      bool user_gesture = false,
-      const GURL& security_origin = GURL());
+      const GURL& security_origin = GURL(),
+      bool user_gesture = false);
 
   content::WebContents* web_contents_;
 
