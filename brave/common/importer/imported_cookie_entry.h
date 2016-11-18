4a8fe6c28aa304d76b6075661aef59bd0c9ff9cf
Anthony Tseng <darkdh@gmail.com>
2016-11-19 07:24:54 +0800

    Moving chrome_importer and imported_cookie_entry to brave

---
+++ brave/common/importer/imported_cookie_entry.h
@@ -0,0 +1,32 @@
+// Copyright (c) 2016 GitHub, Inc.
+// Use of this source code is governed by the MIT license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_COMMON_IMPORTER_IMPORTED_COOKIE_ENTRY_H_
+#define CHROME_COMMON_IMPORTER_IMPORTED_COOKIE_ENTRY_H_
+
+#include "base/strings/string16.h"
+#include "base/time/time.h"
+
+struct ImportedCookieEntry {
+  ImportedCookieEntry() {};
+  ~ImportedCookieEntry() {};
+
+  base::string16 domain;
+
+  base::string16 name;
+
+  base::string16 value;
+
+  base::string16 host;
+
+  base::string16 path;
+
+  base::Time expiry_date;
+
+  bool secure;
+
+  bool httponly;
+};
+
+#endif  // CHROME_COMMON_IMPORTER_IMPORTED_COOKIE_ENTRY_H_
