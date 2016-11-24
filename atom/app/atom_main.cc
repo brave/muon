db8fc5a5467774a2528227a45f85f4a028ea9dac
bridiver <github@brianjohnson.cc>
2016-11-24 11:01:22 -0700

    fix more windows unresolved symbols

--- atom/app/atom_main.cc
+++ atom/app/atom_main.cc
@@ -93,9 +93,9 @@ int APIENTRY wWinMain(HINSTANCE instance, HINSTANCE, wchar_t* cmd, int) {
     }
   }
 
-  if (IsEnvSet("ELECTRON_INTERNAL_CRASH_SERVICE")) {
-    return crash_service::Main(cmd);
-  }
+  // if (IsEnvSet("ELECTRON_INTERNAL_CRASH_SERVICE")) {
+  //   return crash_service::Main(cmd);
+  // }
 
   sandbox::SandboxInterfaceInfo sandbox_info = {0};
   content::InitializeSandboxInfo(&sandbox_info);
