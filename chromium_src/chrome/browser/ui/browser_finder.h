688e05480a64e3bc41b82e340c11dd2e2cea46d1
bridiver <github@brianjohnson.cc>
2016-11-25 10:42:16 -0700

    stub browser finder and window for color chooser aura

---
+++ chromium_src/chrome/browser/ui/browser_finder.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_BROWSER_FINDER_H_
+#define CHROME_BROWSER_UI_BROWSER_FINDER_H_
+
+namespace chrome {
+}  // namespace chrome
+
+#endif  // CHROME_BROWSER_UI_BROWSER_FINDER_H_
