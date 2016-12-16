efcbe03a34f835feb2354b80f8a408be7e9d1ed2
bridiver <github@brianjohnson.cc>
2016-12-15 21:30:33 -0700

    fix 2

--- script/lib/config.py
+++ script/lib/config.py
@@ -24,7 +24,10 @@ def dist_dir():
   return os.path.join(output_dir(), 'dist')
 
 def output_dir():
-  return if (get_target_arch() == 'x64') os.path.join(CHROMIUM_ROOT, 'out', 'Release') else os.path.join(CHROMIUM_ROOT, 'out', 'x86', 'Release')
+  if get_target_arch() == 'x64':
+    return os.path.join(CHROMIUM_ROOT, 'out', 'Release')
+  else
+    return os.path.join(CHROMIUM_ROOT, 'out', 'x86', 'Release')
 
 def electron_package():
   pjson = os.path.join(SOURCE_ROOT, 'package.json')
