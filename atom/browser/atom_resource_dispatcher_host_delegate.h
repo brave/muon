361de3d5e26d7effe55253f8b8f1e28488cb9d4f
Anthony Tseng <darkdh@gmail.com>
2017-01-28 13:50:43 +0800

    Apply changes to "Fix external protocols with <webview>." https://chromium.googlesource.com/chromium/src.git/+/0eb5df657ee01111ab63a0aa235fe04d158f62b3

--- atom/browser/atom_resource_dispatcher_host_delegate.h
+++ atom/browser/atom_resource_dispatcher_host_delegate.h
@@ -19,12 +19,7 @@ class AtomResourceDispatcherHostDelegate
   // content::ResourceDispatcherHostDelegate:
   bool HandleExternalProtocol(
       const GURL& url,
-      int child_id,
-      const content::ResourceRequestInfo::WebContentsGetter&,
-      bool is_main_frame,
-      ui::PageTransition transition,
-      bool has_user_gesture,
-      content::ResourceContext* resource_context) override;
+      content::ResourceRequestInfo* info) override;
   content::ResourceDispatcherHostLoginDelegate* CreateLoginDelegate(
       net::AuthChallengeInfo* auth_info,
       net::URLRequest* request) override;
