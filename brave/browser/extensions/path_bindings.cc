815b915a8d5695ad527151d578997b785d374a91
bridiver <github@brianjohnson.cc>
2017-03-11 13:45:55 -0700

    add missing files for path_bindings auditors @jonathansampson

---
+++ brave/browser/extensions/path_bindings.cc
@@ -0,0 +1,73 @@
+// Copyright (c) 2017 The Brave Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <string>
+
+#include "brave/browser/extensions/path_bindings.h"
+
+#include "base/files/file_path.h"
+#include "extensions/renderer/script_context.h"
+#include "extensions/renderer/v8_helpers.h"
+#include "v8/include/v8.h"
+
+namespace brave {
+
+PathBindings::PathBindings(extensions::ScriptContext* context,
+                          extensions::ModuleSystem::SourceMap* source_map)
+    : extensions::ObjectBackedNativeHandler(context),
+      source_map_(source_map) {
+  RouteFunction("append",
+              base::Bind(&PathBindings::Append, base::Unretained(this)));
+  RouteFunction("dirname",
+              base::Bind(&PathBindings::DirName, base::Unretained(this)));
+  RouteFunction("require",
+              base::Bind(&PathBindings::Require, base::Unretained(this)));
+  // TODO(bridiver) - implement require.paths
+}
+
+PathBindings::~PathBindings() {
+}
+
+void PathBindings::Append(
+    const v8::FunctionCallbackInfo<v8::Value>& args) {
+  if (args.Length() != 2 || !args[0]->IsString() || !args[1]->IsString()) {
+    GetIsolate()->ThrowException(v8::String::NewFromUtf8(
+        GetIsolate(), "Invalid arguments to 'append'"));
+    return;
+  }
+
+  base::FilePath base_path(*v8::String::Utf8Value(args[0]));
+  base::FilePath append_path(*v8::String::Utf8Value(args[1]));
+
+  args.GetReturnValue().Set(v8::String::NewFromUtf8(
+      GetIsolate(), base_path.Append(append_path).AsUTF8Unsafe().c_str()));
+}
+
+void PathBindings::DirName(
+    const v8::FunctionCallbackInfo<v8::Value>& args) {
+  if (args.Length() != 1 || !args[0]->IsString()) {
+    GetIsolate()->ThrowException(v8::String::NewFromUtf8(
+        GetIsolate(), "Invalid arguments to 'dirname'"));
+    return;
+  }
+
+  base::FilePath path(*v8::String::Utf8Value(args[0]));
+
+  args.GetReturnValue().Set(v8::String::NewFromUtf8(
+      GetIsolate(), path.DirName().AsUTF8Unsafe().c_str()));
+}
+
+void PathBindings::Require(
+    const v8::FunctionCallbackInfo<v8::Value>& args) {
+  if (args.Length() != 1 || !args[0]->IsString()) {
+    GetIsolate()->ThrowException(v8::String::NewFromUtf8(
+        GetIsolate(), "Invalid arguments to 'require'"));
+    return;
+  }
+
+  args.GetReturnValue().Set(
+    source_map_->Contains(*v8::String::Utf8Value(args[0])));
+}
+
+}  // namespace brave
---
+++ brave/browser/extensions/path_bindings.h
@@ -0,0 +1,37 @@
+// Copyright (c) 2017 The Brave Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BRAVE_BROWSER_EXTENSIONS_PATH_BINDINGS_H_
+#define BRAVE_BROWSER_EXTENSIONS_PATH_BINDINGS_H_
+
+#include <string>
+#include <utility>
+
+#include "base/compiler_specific.h"
+#include "base/macros.h"
+#include "extensions/renderer/module_system.h"
+#include "extensions/renderer/object_backed_native_handler.h"
+#include "v8/include/v8.h"
+
+namespace brave {
+
+class PathBindings : public extensions::ObjectBackedNativeHandler {
+ public:
+  PathBindings(extensions::ScriptContext* context,
+      extensions::ModuleSystem::SourceMap* source_map);
+  ~PathBindings() override;
+
+ private:
+  void Append(const v8::FunctionCallbackInfo<v8::Value>& args);
+  void DirName(const v8::FunctionCallbackInfo<v8::Value>& args);
+  void Require(const v8::FunctionCallbackInfo<v8::Value>& args);
+
+  const extensions::ModuleSystem::SourceMap* source_map_;
+
+  DISALLOW_COPY_AND_ASSIGN(PathBindings);
+};
+
+}  // namespace brave
+
+#endif  // BRAVE_BROWSER_EXTENSIONS_PATH_BINDINGS_H_
