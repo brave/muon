1c389d489044d90fbc0fed9115ee3b59d44bee87
bridiver <github@brianjohnson.cc>
2017-03-10 17:46:48 -0700

    lint fixes

--- atom/browser/native_window_mac.h
+++ atom/browser/native_window_mac.h
@@ -106,7 +106,8 @@ class NativeWindowMac : public NativeWindow,
   // content::WebContentsObserver:
   void RenderViewHostChanged(content::RenderViewHost* old_host,
                              content::RenderViewHost* new_host) override;
-  void DidFirstPaintAfterLoad(content::RenderWidgetHost* render_widget_host) override;
+  void DidFirstPaintAfterLoad(
+      content::RenderWidgetHost* render_widget_host) override;
 
   // Refresh the DraggableRegion views.
   void UpdateDraggableRegionViews() {
