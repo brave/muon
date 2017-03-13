dd1b705a365b24779daca1ace1099a3900fe1fe4
Anthony Tseng <darkdh@gmail.com>
2017-03-12 23:43:22 -0400

    Add missing in_prcoess_importer_bridge changes to "Convert profile import IPCs to Mojo" https://chromium.googlesource.com/chromium/src.git/+/1d83f16c4e8a49c614479bbd5e74f16aad7c8232

--- chromium_src/chrome/browser/importer/in_process_importer_bridge.cc
+++ chromium_src/chrome/browser/importer/in_process_importer_bridge.cc
@@ -2,26 +2,19 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/**
- * This is not a straight copy from chromium src, in particular
- * some functionality is removed.
- * This was originally forked with 52.0.2743.116.  Diff against
- * a version of that file for a full list of changes.
- */
-
 #include "chrome/browser/importer/in_process_importer_bridge.h"
 
 #include <stddef.h>
 
-#include "base/bind.h"
 #include "base/files/file_util.h"
 #include "base/macros.h"
+#include "base/memory/ptr_util.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "brave/common/importer/imported_cookie_entry.h"
 #include "build/build_config.h"
 #include "chrome/browser/importer/external_process_importer_host.h"
-// #include "chrome/browser/search_engines/ui_thread_search_terms_data.h"
+#include "chrome/browser/search_engines/ui_thread_search_terms_data.h"
 #include "chrome/common/importer/imported_bookmark_entry.h"
 #include "chrome/common/importer/importer_autofill_form_data_entry.h"
 #include "components/autofill/core/browser/webdata/autofill_entry.h"
@@ -30,7 +23,6 @@
 #include "components/search_engines/template_url.h"
 #include "components/search_engines/template_url_parser.h"
 #include "components/search_engines/template_url_prepopulate_data.h"
-#include "content/public/browser/browser_thread.h"
 #include "ui/base/l10n/l10n_util.h"
 
 #if defined(OS_WIN)
@@ -79,8 +71,6 @@ history::VisitSource ConvertImporterVisitSourceToHistoryVisitSource(
 
 }  // namespace
 
-using content::BrowserThread;
-
 namespace {
 
 // FirefoxURLParameterFilter is used to remove parameter mentioning Firefox from
@@ -106,71 +96,6 @@ class FirefoxURLParameterFilter : public TemplateURLParser::ParameterFilter {
   DISALLOW_COPY_AND_ASSIGN(FirefoxURLParameterFilter);
 };
 
-/*
-// Attempts to create a TemplateURL from the provided data. |title| is optional.
-// If TemplateURL creation fails, returns NULL.
-// This function transfers ownership of the created TemplateURL to the caller.
-TemplateURL* CreateTemplateURL(const base::string16& url,
-                               const base::string16& keyword,
-                               const base::string16& title) {
-  if (url.empty() || keyword.empty())
-    return NULL;
-  TemplateURLData data;
-  data.SetKeyword(keyword);
-  // We set short name by using the title if it exists.
-  // Otherwise, we use the shortcut.
-  data.SetShortName(title.empty() ? keyword : title);
-  data.SetURL(TemplateURLRef::DisplayURLToURLRef(url));
-  return new TemplateURL(data);
-}
-
-// Parses the OpenSearch XML files in |xml_files| and populates |search_engines|
-// with the resulting TemplateURLs.
-void ParseSearchEnginesFromFirefoxXMLData(
-    const std::vector<std::string>& xml_data,
-    std::vector<TemplateURL*>* search_engines) {
-  DCHECK(search_engines);
-  typedef std::map<std::string, TemplateURL*> SearchEnginesMap;
-  SearchEnginesMap search_engine_for_url;
-  FirefoxURLParameterFilter param_filter;
-  // The first XML file represents the default search engine in Firefox 3, so we
-  // need to keep it on top of the list.
-  SearchEnginesMap::const_iterator default_turl = search_engine_for_url.end();
-  for (std::vector<std::string>::const_iterator xml_iter =
-           xml_data.begin(); xml_iter != xml_data.end(); ++xml_iter) {
-    TemplateURL* template_url = TemplateURLParser::Parse(
-        UIThreadSearchTermsData(NULL), true,
-        xml_iter->data(), xml_iter->length(), &param_filter);
-    if (template_url) {
-      SearchEnginesMap::iterator iter =
-          search_engine_for_url.find(template_url->url());
-      if (iter == search_engine_for_url.end()) {
-        iter = search_engine_for_url.insert(
-            std::make_pair(template_url->url(), template_url)).first;
-      } else {
-        // We have already found a search engine with the same URL.  We give
-        // priority to the latest one found, as GetSearchEnginesXMLFiles()
-        // returns a vector with first Firefox default search engines and then
-        // the user's ones.  We want to give priority to the user ones.
-        delete iter->second;
-        iter->second = template_url;
-      }
-      if (default_turl == search_engine_for_url.end())
-        default_turl = iter;
-    }
-  }
-
-  // Put the results in the |search_engines| vector.
-  for (SearchEnginesMap::iterator t_iter = search_engine_for_url.begin();
-       t_iter != search_engine_for_url.end(); ++t_iter) {
-    if (t_iter == default_turl)
-      search_engines->insert(search_engines->begin(), default_turl->second);
-    else
-      search_engines->push_back(t_iter->second);
-  }
-}
-*/
-
 }  // namespace
 
 InProcessImporterBridge::InProcessImporterBridge(
@@ -182,16 +107,11 @@ InProcessImporterBridge::InProcessImporterBridge(
 void InProcessImporterBridge::AddBookmarks(
     const std::vector<ImportedBookmarkEntry>& bookmarks,
     const base::string16& first_folder_name) {
-  BrowserThread::PostTask(
-      BrowserThread::UI, FROM_HERE,
-      base::Bind(&ProfileWriter::AddBookmarks, writer_, bookmarks,
-                 first_folder_name));
+  writer_->AddBookmarks(bookmarks, first_folder_name);
 }
 
 void InProcessImporterBridge::AddHomePage(const GURL& home_page) {
-  BrowserThread::PostTask(
-      BrowserThread::UI, FROM_HERE,
-      base::Bind(&ProfileWriter::AddHomepage, writer_, home_page));
+  writer_->AddHomepage(home_page);
 }
 
 #if defined(OS_WIN)
@@ -202,18 +122,13 @@ void InProcessImporterBridge::AddIE7PasswordInfo(
   ie7_password_info.encrypted_data = password_info.encrypted_data;
   ie7_password_info.date_created = password_info.date_created;
 
-  BrowserThread::PostTask(
-      BrowserThread::UI, FROM_HERE,
-      base::Bind(&ProfileWriter::AddIE7PasswordInfo, writer_,
-                 ie7_password_info));
+  writer_->AddIE7PasswordInfo(ie7_password_info);
 }
 #endif  // OS_WIN
 
 void InProcessImporterBridge::SetFavicons(
     const favicon_base::FaviconUsageDataList& favicons) {
-  BrowserThread::PostTask(
-      BrowserThread::UI, FROM_HERE,
-      base::Bind(&ProfileWriter::AddFavicons, writer_, favicons));
+  writer_->AddFavicons(favicons);
 }
 
 void InProcessImporterBridge::SetHistoryItems(
@@ -223,54 +138,21 @@ void InProcessImporterBridge::SetHistoryItems(
       ConvertImporterURLRowsToHistoryURLRows(rows);
   history::VisitSource converted_visit_source =
       ConvertImporterVisitSourceToHistoryVisitSource(visit_source);
-  BrowserThread::PostTask(BrowserThread::UI,
-                          FROM_HERE,
-                          base::Bind(&ProfileWriter::AddHistoryPage,
-                                     writer_,
-                                     converted_rows,
-                                     converted_visit_source));
+  writer_->AddHistoryPage(converted_rows, converted_visit_source);
 }
 
 void InProcessImporterBridge::SetKeywords(
     const std::vector<importer::SearchEngineInfo>& search_engines,
     bool unique_on_host_and_path) {
-  /*
-  ScopedVector<TemplateURL> owned_template_urls;
-  for (const auto& search_engine : search_engines) {
-    TemplateURL* owned_template_url =
-        CreateTemplateURL(search_engine.url,
-                          search_engine.keyword,
-                          search_engine.display_name);
-    if (owned_template_url)
-      owned_template_urls.push_back(owned_template_url);
-  }
-  BrowserThread::PostTask(BrowserThread::UI, FROM_HERE,
-      base::Bind(&ProfileWriter::AddKeywords, writer_,
-                 base::Passed(&owned_template_urls), unique_on_host_and_path));
-  */
 }
 
 void InProcessImporterBridge::SetFirefoxSearchEnginesXMLData(
     const std::vector<std::string>& search_engine_data) {
-  /*
-  std::vector<TemplateURL*> search_engines;
-  ParseSearchEnginesFromFirefoxXMLData(search_engine_data, &search_engines);
-
-  ScopedVector<TemplateURL> owned_template_urls;
-  std::copy(search_engines.begin(), search_engines.end(),
-            std::back_inserter(owned_template_urls));
-
-  BrowserThread::PostTask(BrowserThread::UI, FROM_HERE,
-      base::Bind(&ProfileWriter::AddKeywords, writer_,
-                 base::Passed(&owned_template_urls), true));
-  */
 }
 
 void InProcessImporterBridge::SetPasswordForm(
     const autofill::PasswordForm& form) {
-  BrowserThread::PostTask(
-      BrowserThread::UI, FROM_HERE,
-      base::Bind(&ProfileWriter::AddPasswordForm, writer_, form));
+  writer_->AddPasswordForm(form);
 }
 
 void InProcessImporterBridge::SetAutofillFormData(
@@ -283,44 +165,28 @@ void InProcessImporterBridge::SetAutofillFormData(
         entries[i].last_used));
   }
 
-  BrowserThread::PostTask(BrowserThread::UI,
-                          FROM_HERE,
-                          base::Bind(&ProfileWriter::AddAutofillFormDataEntries,
-                                     writer_,
-                                     autofill_entries));
+  writer_->AddAutofillFormDataEntries(autofill_entries);
 }
 
 void InProcessImporterBridge::SetCookies(
     const std::vector<ImportedCookieEntry>& cookies) {
-  BrowserThread::PostTask(
-      BrowserThread::UI, FROM_HERE,
-      base::Bind(&ProfileWriter::AddCookies, writer_, cookies));
+  writer_->AddCookies(cookies);
 }
 
 void InProcessImporterBridge::NotifyStarted() {
-  BrowserThread::PostTask(
-      BrowserThread::UI, FROM_HERE,
-      base::Bind(&ExternalProcessImporterHost::NotifyImportStarted, host_));
+  host_->NotifyImportStarted();
 }
 
 void InProcessImporterBridge::NotifyItemStarted(importer::ImportItem item) {
-  BrowserThread::PostTask(
-      BrowserThread::UI, FROM_HERE,
-      base::Bind(&ExternalProcessImporterHost::NotifyImportItemStarted,
-                 host_, item));
+  host_->NotifyImportItemStarted(item);
 }
 
 void InProcessImporterBridge::NotifyItemEnded(importer::ImportItem item) {
-  BrowserThread::PostTask(
-      BrowserThread::UI, FROM_HERE,
-      base::Bind(&ExternalProcessImporterHost::NotifyImportItemEnded,
-                 host_, item));
+  host_->NotifyImportItemEnded(item);
 }
 
 void InProcessImporterBridge::NotifyEnded() {
-  BrowserThread::PostTask(
-      BrowserThread::UI, FROM_HERE,
-      base::Bind(&ExternalProcessImporterHost::NotifyImportEnded, host_));
+  host_->NotifyImportEnded();
 }
 
 base::string16 InProcessImporterBridge::GetLocalizedString(int message_id) {
