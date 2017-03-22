e434d1705dabc650e91dc72df931e09821aadd49
Brian R. Bondy <netzen@gmail.com>
2017-03-21 23:25:17 -0400

    Remove WebCache.h
    See https://chromium.googlesource.com/chromium/src.git/+/584e0be921ffc96b1b8b400dece9ed18a6af1931%5E%21/

--- atom/common/native_mate_converters/blink_converter.h
+++ atom/common/native_mate_converters/blink_converter.h
@@ -6,7 +6,6 @@
 #define ATOM_COMMON_NATIVE_MATE_CONVERTERS_BLINK_CONVERTER_H_
 
 #include "native_mate/converter.h"
-#include "third_party/WebKit/public/web/WebCache.h"
 #include "third_party/WebKit/public/web/WebContextMenuData.h"
 
 namespace blink {
@@ -99,20 +98,6 @@ template<>
 struct Converter<blink::WebContextMenuData::InputFieldType> {
   static v8::Local<v8::Value> ToV8(v8::Isolate* isolate,
       const blink::WebContextMenuData::InputFieldType& in);
-};
-
-template<>
-struct Converter<blink::WebCache::ResourceTypeStat> {
-  static v8::Local<v8::Value> ToV8(
-      v8::Isolate* isolate,
-      const blink::WebCache::ResourceTypeStat& stat);
-};
-
-template<>
-struct Converter<blink::WebCache::ResourceTypeStats> {
-  static v8::Local<v8::Value> ToV8(
-      v8::Isolate* isolate,
-      const blink::WebCache::ResourceTypeStats& stats);
 };
 
 v8::Local<v8::Value> EditFlagsToV8(v8::Isolate* isolate, int editFlags);
