38e6b0b8568c9339ae263b5c49ebcc0c6f7fb7eb
Brian R. Bondy <netzen@gmail.com>
2017-03-22 01:43:24 -0400

    Remove FundamentalValue
    See https://chromium.googlesource.com/chromium/src.git/+/23972357b3fb5591905c94528ee39ae1d1351f34%5E%21/

--- chromium_src/chrome/browser/custom_handlers/protocol_handler_registry.cc
+++ chromium_src/chrome/browser/custom_handlers/protocol_handler_registry.cc
@@ -772,7 +772,7 @@ base::Value* ProtocolHandlerRegistry::EncodeRegisteredHandlers() {
          j != i->second.end(); ++j) {
       std::unique_ptr<base::DictionaryValue> encoded = j->Encode();
       if (IsDefault(*j)) {
-        encoded->Set("default", new base::FundamentalValue(true));
+        encoded->Set("default", new base::Value(true));
       }
       protocol_handlers->Append(std::move(encoded));
     }
