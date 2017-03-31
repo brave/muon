bea705d34968ec02425c9f1b9825d465ae97f6dd
bridiver <github@brianjohnson.cc>
2017-03-31 15:00:51 -0700

    patch updates

--- chromium_src/chrome/browser/engagement/site_engagement_service.cc
+++ chromium_src/chrome/browser/engagement/site_engagement_service.cc
@@ -4,6 +4,11 @@
 
 #include "chrome/browser/engagement/site_engagement_service.h"
 
+#include "base/memory/ptr_util.h"
+#include "base/time/clock.h"
+#include "base/time/default_clock.h"
+#include "chrome/browser/engagement/site_engagement_score.h"
+
 // static
 bool SiteEngagementService::IsEnabled() {
   return false;
@@ -12,4 +17,13 @@ bool SiteEngagementService::IsEnabled() {
 // static
 SiteEngagementService* SiteEngagementService::Get(Profile* profile) {
   return nullptr;
+}
+
+// static
+double SiteEngagementService::GetScoreFromSettings(
+    HostContentSettingsMap* settings,
+    const GURL& origin) {
+  auto clock = base::MakeUnique<base::DefaultClock>();
+  return SiteEngagementScore(clock.get(), origin, settings)
+      .GetScore();
 }
