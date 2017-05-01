24d192e654a73083c545f6c15f36aeceb3eaae1f
Brian R. Bondy <netzen@gmail.com>
2017-05-01 19:42:42 -0400

    RulesetService -> ContentRulesetService
    https://chromium.googlesource.com/chromium/src.git/+/8a923d69f115eb6c5e48f548feea0afa30d8f4b9%5E%21/

--- chromium_src/chrome/browser/browser_process_impl.cc
+++ chromium_src/chrome/browser/browser_process_impl.cc
@@ -397,7 +397,7 @@ BrowserProcessImpl::safe_browsing_detection_service() {
   return nullptr;
 }
 
-subresource_filter::RulesetService*
+subresource_filter::ContentRulesetService*
 BrowserProcessImpl::subresource_filter_ruleset_service() {
   NOTIMPLEMENTED();
   return nullptr;
