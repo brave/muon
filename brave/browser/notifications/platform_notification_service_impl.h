fa30c02200204814a2b7fd0784c2fa0276ab7485
Brian R. Bondy <netzen@gmail.com>
2017-05-01 20:02:36 -0400

    Make GetDisplayedNotifications asynchronous
    https://chromium.googlesource.com/chromium/src.git/+/bfe768358163d78edf9b3a48bef284427fb4d380%5E%21/

--- brave/browser/notifications/platform_notification_service_impl.h
+++ brave/browser/notifications/platform_notification_service_impl.h
@@ -51,9 +51,9 @@ class PlatformNotificationServiceImpl
       const content::NotificationResources& notification_resources) override;
   void ClosePersistentNotification(content::BrowserContext* browser_context,
                                    const std::string& notification_id) override;
-  bool GetDisplayedNotifications(
+  void GetDisplayedNotifications(
       content::BrowserContext* browser_context,
-      std::set<std::string>* displayed_notifications) override;
+      const DisplayedNotificationsCallback& callback) override;
 
  private:
   friend struct base::DefaultSingletonTraits<PlatformNotificationServiceImpl>;
