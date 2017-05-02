dd1ce3b85320b955bd617eb3cb9735817000cb83
Brian R. Bondy <netzen@gmail.com>
2017-05-02 00:15:31 -0400

    Pass guest Destroy also_delete arg

--- atom/browser/extensions/tab_helper.cc
+++ atom/browser/extensions/tab_helper.cc
@@ -147,7 +147,7 @@ content::WebContents* TabHelper::CreateTab(content::WebContents* owner,
 void TabHelper::DestroyTab(content::WebContents* tab) {
   auto guest = brave::TabViewGuest::FromWebContents(tab);
   DCHECK(guest);
-  guest->Destroy();
+  guest->Destroy(true);
 }
 
 // static
