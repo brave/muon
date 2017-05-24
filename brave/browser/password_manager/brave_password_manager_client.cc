fa39fa2159b6d6b0ca02cfd751509620d98b0ebb
Brian R. Bondy <netzen@gmail.com>
2017-05-23 21:33:59 -0400

    Implement pure virtual function GetPasswordProtectionService
    https://chromium.googlesource.com/chromium/src/+/4522d139cf6832a2f3393d374dd9b025902935dc%5E%21/

--- brave/browser/password_manager/brave_password_manager_client.cc
+++ brave/browser/password_manager/brave_password_manager_client.cc
@@ -340,6 +340,11 @@ BravePasswordManagerClient::GetPasswordStore() const {
              profile_, ServiceAccessType::EXPLICIT_ACCESS).get();
 }
 
+safe_browsing::PasswordProtectionService*
+BravePasswordManagerClient::GetPasswordProtectionService() const {
+  return nullptr;
+}
+
 password_manager::PasswordSyncState
 BravePasswordManagerClient::GetPasswordSyncState() const {
   return password_manager_util::GetPasswordSyncState(nullptr);
