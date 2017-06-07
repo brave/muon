5f57b6c337a718ca9dc7c0ff58efeda9ee2193ae
Henrique Ferreiro <hferreiro@igalia.com>
2017-06-07 13:17:15 +0200

    Extensions are always isolated now
    Chromium commit abed2a56f122e7d6ca95e37a42c0c14b73a85a35

--- brave/browser/brave_content_browser_client.cc
+++ brave/browser/brave_content_browser_client.cc
@@ -411,7 +411,6 @@ void BraveContentBrowserClient::AppendExtraCommandLineSwitches(
       extensions::switches::kEnableEmbeddedExtensionOptions,
       extensions::switches::kEnableExperimentalExtensionApis,
       extensions::switches::kExtensionsOnChromeURLs,
-      extensions::switches::kIsolateExtensions,
       extensions::switches::kWhitelistedExtensionID,
 #endif
     };
