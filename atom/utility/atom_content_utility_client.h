ab6ed2b362e7e6fa10a0e0feffe788a61e30498f
Henrique Ferreiro <hferreiro@igalia.com>
2017-06-07 14:08:59 +0200

    Rework AtomContentUtilityClient
    Chromium commits 7f9189cb86fdc65ca77995d28ee9604a3c5a8312 and
    04181130ec65e64d013c43d3e1bee71237c5fa75 and
    c8702c4cdecf0859c34a5ae564c89463ac727967 and
    3b491f68e9be76a22f54208d6c1a3531eb4d8386 and
    11c5ec813536507048ad1c54d04840a522fa5748

--- atom/utility/atom_content_utility_client.h
+++ atom/utility/atom_content_utility_client.h
@@ -5,14 +5,10 @@
 #ifndef ATOM_UTILITY_ATOM_CONTENT_UTILITY_CLIENT_H_
 #define ATOM_UTILITY_ATOM_CONTENT_UTILITY_CLIENT_H_
 
-#include <set>
-#include <string>
+#include <memory>
 #include <vector>
 
-#include "base/compiler_specific.h"
-#include "base/memory/scoped_vector.h"
 #include "content/public/utility/content_utility_client.h"
-#include "ipc/ipc_platform_file.h"
 
 namespace base {
 class FilePath;
@@ -32,25 +28,19 @@ class AtomContentUtilityClient : public content::ContentUtilityClient {
   AtomContentUtilityClient();
   ~AtomContentUtilityClient() override;
 
+  // content::ContentUtilityClient:
   void UtilityThreadStarted() override;
   bool OnMessageReceived(const IPC::Message& message) override;
-  void ExposeInterfacesToBrowser(
-      service_manager::InterfaceRegistry* registry) override;
 
   static void PreSandboxStartup();
 
  private:
-  void OnStartupPing();
-
-  typedef ScopedVector<UtilityMessageHandler> Handlers;
+  // IPC message handlers.
+  using Handlers = std::vector<std::unique_ptr<UtilityMessageHandler>>;
   Handlers handlers_;
 
-  // Flag to enable whitelisting.
-  bool filter_messages_;
-  // A list of message_ids to filter.
-  std::set<int> message_id_whitelist_;
-  // Maximum IPC msg size (default to kMaximumMessageSize; override for testing)
-  static int64_t max_ipc_message_size_;
+  // True if the utility process runs with elevated privileges.
+  bool utility_process_running_elevated_;
 
   DISALLOW_COPY_AND_ASSIGN(AtomContentUtilityClient);
 };
