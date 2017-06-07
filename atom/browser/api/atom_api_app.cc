f23b22d4b4c6ce03183c0c3249fe712817e18c1b
Henrique Ferreiro <hferreiro@igalia.com>
2017-06-07 11:15:34 +0200

    Update atom::api::App:SelectClientCertificate parameters
    Chromium commit 7ed243fc89fb72230c47f59742128add62ae1d83

--- atom/browser/api/atom_api_app.cc
+++ atom/browser/api/atom_api_app.cc
@@ -637,22 +637,19 @@ void App::AllowCertificateError(
 void App::SelectClientCertificate(
     content::WebContents* web_contents,
     net::SSLCertRequestInfo* cert_request_info,
+    net::CertificateList client_certs,
     std::unique_ptr<content::ClientCertificateDelegate> delegate) {
   std::shared_ptr<content::ClientCertificateDelegate>
       shared_delegate(delegate.release());
-  bool prevent_default =
-      Emit("select-client-certificate",
-           WebContents::CreateFrom(isolate(), web_contents),
-           cert_request_info->host_and_port.ToString(),
-           cert_request_info->client_certs,
-           base::Bind(&OnClientCertificateSelected,
-                      isolate(),
-                      shared_delegate));
+  bool prevent_default = Emit(
+      "select-client-certificate",
+      WebContents::CreateFrom(isolate(), web_contents),
+      cert_request_info->host_and_port.ToString(), client_certs,
+      base::Bind(&OnClientCertificateSelected, isolate(), shared_delegate));
 
   // Default to first certificate from the platform store.
   if (!prevent_default)
-    shared_delegate->ContinueWithCertificate(
-        cert_request_info->client_certs[0].get());
+    shared_delegate->ContinueWithCertificate(client_certs[0].get());
 }
 
 void App::OnMaxBandwidthChanged(
