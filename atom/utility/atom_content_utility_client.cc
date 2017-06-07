627c9afb2e66c55345c73d80e47f0c5305d8bfc1
Henrique Ferreiro <hferreiro@igalia.com>
2017-06-07 14:08:59 +0200

    Rework AtomContentUtilityClient
    Chromium commits 7f9189cb86fdc65ca77995d28ee9604a3c5a8312 and
    04181130ec65e64d013c43d3e1bee71237c5fa75 and
    c8702c4cdecf0859c34a5ae564c89463ac727967 and
    3b491f68e9be76a22f54208d6c1a3531eb4d8386 and
    11c5ec813536507048ad1c54d04840a522fa5748

--- atom/utility/atom_content_utility_client.cc
+++ atom/utility/atom_content_utility_client.cc
@@ -13,8 +13,11 @@
 #include "brave/utility/brave_profile_import_handler.h"
 #include "chrome/common/chrome_utility_messages.h"
 #include "chrome/common/resource_usage_reporter.mojom.h"
+#include "chrome/utility/extensions/extensions_handler.h"
 #include "chrome/utility/utility_message_handler.h"
 #include "content/public/common/content_switches.h"
+#include "content/public/common/service_manager_connection.h"
+#include "content/public/common/simple_connection_filter.h"
 #include "content/public/utility/utility_thread.h"
 #include "extensions/features/features.h"
 #include "ipc/ipc_channel.h"
@@ -23,7 +26,7 @@
 #include "net/proxy/mojo_proxy_resolver_factory_impl.h"
 #include "net/proxy/proxy_resolver_v8.h"
 #include "printing/features/features.h"
-#include "services/service_manager/public/cpp/interface_registry.h"
+#include "services/service_manager/public/cpp/binder_registry.h"
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
 #include "chrome/common/extensions/chrome_extensions_client.h"
@@ -74,68 +77,61 @@ void CreateResourceUsageReporter(
 
 }  // namespace
 
-int64_t AtomContentUtilityClient::max_ipc_message_size_ =
-    IPC::Channel::kMaximumMessageSize;
-
 AtomContentUtilityClient::AtomContentUtilityClient()
-    : filter_messages_(false) {
+    : utility_process_running_elevated_(false) {
 #if BUILDFLAG(ENABLE_PRINT_PREVIEW) || \
     (BUILDFLAG(ENABLE_BASIC_PRINTING) && defined(OS_WIN))
   handlers_.push_back(new printing::PrintingHandler());
 #endif
 }
 
-AtomContentUtilityClient::~AtomContentUtilityClient() {
-}
+AtomContentUtilityClient::~AtomContentUtilityClient() = default;
 
 void AtomContentUtilityClient::UtilityThreadStarted() {
 #if BUILDFLAG(ENABLE_EXTENSIONS)
   extensions::utility_handler::UtilityThreadStarted();
 #endif
+
+  base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
+  if (command_line->HasSwitch(switches::kUtilityProcessRunningElevated))
+    utility_process_running_elevated_ = true;
+
+  content::ServiceManagerConnection* connection =
+      content::ChildThread::Get()->GetServiceManagerConnection();
+
+  // NOTE: Some utility process instances are not connected to the Service
+  // Manager. Nothing left to do in that case.
+  if (!connection)
+    return;
+
+  auto registry = base::MakeUnique<service_manager::BinderRegistry>();
+  // If our process runs with elevated privileges, only add elevated Mojo
+  // interfaces to the interface registry.
+  if (!utility_process_running_elevated_) {
+    registry->AddInterface<net::interfaces::ProxyResolverFactory>(
+        base::Bind(CreateProxyResolverFactory),
+        base::ThreadTaskRunnerHandle::Get());
+    registry->AddInterface(base::Bind(CreateResourceUsageReporter),
+                           base::ThreadTaskRunnerHandle::Get());
+    registry->AddInterface(base::Bind(&BraveProfileImportHandler::Create),
+                           base::ThreadTaskRunnerHandle::Get());
+  }
+
+  connection->AddConnectionFilter(
+      base::MakeUnique<content::SimpleConnectionFilter>(std::move(registry)));
 }
 
 bool AtomContentUtilityClient::OnMessageReceived(
     const IPC::Message& message) {
-  if (filter_messages_ &&
-      !base::ContainsKey(message_id_whitelist_, message.type()))
+  if (utility_process_running_elevated_)
     return false;
 
-  bool handled = true;
-  IPC_BEGIN_MESSAGE_MAP(AtomContentUtilityClient, message)
-    IPC_MESSAGE_UNHANDLED(handled = false)
-  IPC_END_MESSAGE_MAP()
-
-  if (handled)
-    return true;
-
-  for (auto* handler : handlers_) {
-    // At least one of the utility process handlers adds a new handler to
-    // |handlers_| when it handles a message. This causes any iterator over
-    // |handlers_| to become invalid. Therefore, it is necessary to break the
-    // loop at this point instead of evaluating it as a loop condition (if the
-    // for loop was using iterators explicitly, as originally done).
+  for (const auto& handler : handlers_) {
     if (handler->OnMessageReceived(message))
       return true;
   }
 
   return false;
-}
-
-void AtomContentUtilityClient::ExposeInterfacesToBrowser(
-    service_manager::InterfaceRegistry* registry) {
-  // When the utility process is running with elevated privileges, we need to
-  // filter messages so that only a whitelist of IPCs can run. In Mojo, there's
-  // no way of filtering individual messages. Instead, we can avoid adding
-  // non-whitelisted Mojo services to the service_manager::InterfaceRegistry.
-  // TODO(amistry): Use a whitelist once the whistlisted IPCs have been
-  // converted to Mojo.
-  if (filter_messages_)
-    return;
-
-  registry->AddInterface<net::interfaces::ProxyResolverFactory>(
-      base::Bind(CreateProxyResolverFactory));
-  registry->AddInterface(base::Bind(CreateResourceUsageReporter));
-  registry->AddInterface(base::Bind(&BraveProfileImportHandler::Create));
 }
 
 // static
