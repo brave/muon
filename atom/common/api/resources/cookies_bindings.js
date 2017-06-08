405eb0c7e3e22e1092939850cb8ab7df1f743568
kevinlawler <git@kevinlawler.com>
2017-06-08 15:32:06 -0700

    Add chrome.cookies.{getAll, set, get} for Pinterest Save Button extension (commit split from Pinterest key)
    brave/browser-laptop#6366
    brave/browser-laptop#6262
    brave/browser-laptop#8389

--- atom/common/api/resources/cookies_bindings.js
+++ atom/common/api/resources/cookies_bindings.js
@@ -1,12 +1,48 @@
 var binding = require('binding').Binding.create('cookies')
+var ipc = require('ipc_utils')
+var lastError = require('lastError')
 
 binding.registerCustomHook(function (bindingsAPI, extensionId) {
   var apiFunctions = bindingsAPI.apiFunctions
   // var cookies = bindingsAPI.compiledApi
 
   apiFunctions.setHandleRequest('getAll', function (details, cb) {
-    var nothing = []
-    cb(nothing)
+    var responseId = ipc.guid()
+    cb && ipc.once('chrome-cookies-getall-response-' + responseId, function (evt, error, cookielist) {
+      if (error) {
+        lastError.run('cookies.getall', error, '', () => { cb(null) })
+      } else {
+        cb(cookielist)
+      }
+    })
+
+    ipc.send('chrome-cookies-getall', responseId, details)
+  })
+
+  apiFunctions.setHandleRequest('get', function (details, cb) {
+    var responseId = ipc.guid()
+    cb && ipc.once('chrome-cookies-get-response-' + responseId, function (evt, error, cookie) {
+      if (error) {
+        lastError.run('cookies.get', error, '', () => { cb(null) })
+      } else {
+        cb(cookie)
+      }
+    })
+
+    ipc.send('chrome-cookies-get', responseId, details)
+  })
+
+  apiFunctions.setHandleRequest('set', function (details, cb) {
+    var responseId = ipc.guid()
+    cb && ipc.once('chrome-cookies-set-response-' + responseId, function (evt, error, cookie) {
+      if (error) {
+        lastError.run('cookies.set', error, '', () => { cb(null) })
+      } else {
+        cb(cookie)
+      }
+    })
+
+    ipc.send('chrome-cookies-set', responseId, details)
   })
 })
 
