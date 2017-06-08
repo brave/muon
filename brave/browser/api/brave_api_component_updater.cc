b113a2e836795d2100e83910ea721777fd2f5ec2
kevinlawler <git@kevinlawler.com>
2017-06-08 15:35:38 -0700

    Register Pinterest component + key
    brave/browser-laptop#6366
    brave/browser-laptop#6262
    brave/browser-laptop#8389
    brave/muon/pull/200

--- brave/browser/api/brave_api_component_updater.cc
+++ brave/browser/api/brave_api_component_updater.cc
@@ -142,6 +142,9 @@ void ComponentUpdater::RegisterComponent(const std::string& component_id) {
   } else if (component_id == kHoneyId) {
     RegisterComponentForUpdate(
         kHoneyPublicKeyStr, registered_callback, ready_callback);
+  } else if (component_id == kPinterestId) {
+    RegisterComponentForUpdate(
+        kPinterestPublicKeyStr, registered_callback, ready_callback);
   } else if (component_id == kWidevineId) {
     brave::RegisterWidevineCdmComponent(
         g_browser_process->component_updater(),
