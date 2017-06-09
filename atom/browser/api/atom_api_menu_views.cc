64a6fa000e9aa18e1b0779f1b8a7198968108d54
Henrique Ferreiro <hferreiro@igalia.com>
2017-06-09 10:58:30 +0200

    views::MenuRunner no longer has a return value
    Chromium commit 5ab7c2d09320aa6f4fc6ba0118908ac7e378e77b

--- atom/browser/api/atom_api_menu_views.cc
+++ atom/browser/api/atom_api_menu_views.cc
@@ -44,8 +44,7 @@ void MenuViews::PopupAt(
   }
 
   int flags = MenuRunner::CONTEXT_MENU |
-              MenuRunner::HAS_MNEMONICS |
-              MenuRunner::ASYNC;
+              MenuRunner::HAS_MNEMONICS;
 
   // Don't emit unresponsive event when showing menu.
   atom::UnresponsiveSuppressor suppressor;
@@ -56,12 +55,10 @@ void MenuViews::PopupAt(
       &MenuViews::ClosePopupAt, weak_factory_.GetWeakPtr(), window_id);
   menu_runners_[window_id] = std::unique_ptr<MenuRunner>(new MenuRunner(
       model(), flags, close_callback));
-  ignore_result(menu_runners_[window_id]->RunMenuAt(
-      static_cast<NativeWindowViews*>(window->window())->widget(),
-      NULL,
-      gfx::Rect(location, gfx::Size()),
-      views::MENU_ANCHOR_TOPLEFT,
-      ui::MENU_SOURCE_MOUSE));
+  menu_runners_[window_id]->RunMenuAt(
+      static_cast<NativeWindowViews*>(window->window())->widget(), NULL,
+      gfx::Rect(location, gfx::Size()), views::MENU_ANCHOR_TOPLEFT,
+      ui::MENU_SOURCE_MOUSE);
 }
 
 void MenuViews::ClosePopupAt(int32_t window_id) {
