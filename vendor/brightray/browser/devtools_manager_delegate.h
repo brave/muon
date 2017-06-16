19c391b29f36b83cb2d9f17618739b43f8b4c216
Henrique Ferreiro <hferreiro@igalia.com>
2017-06-16 12:21:48 +0200

    Update DevToolsManagerDelegate::GetTarget{Type,Title} parameter
    Chromium commit dccd667e6f787a2ef0d123ee9b873732d723d76f

--- vendor/brightray/browser/devtools_manager_delegate.h
+++ vendor/brightray/browser/devtools_manager_delegate.h
@@ -28,9 +28,9 @@ class DevToolsManagerDelegate : public content::DevToolsManagerDelegate,
   void Inspect(content::DevToolsAgentHost* agent_host) override {}
   base::DictionaryValue* HandleCommand(content::DevToolsAgentHost* agent_host,
                                        base::DictionaryValue* command) override;
-  std::string GetTargetType(content::RenderFrameHost* host) override
+  std::string GetTargetType(content::WebContents* web_contents) override
     {return std::string();}
-  std::string GetTargetTitle(content::RenderFrameHost* host) override
+  std::string GetTargetTitle(content::WebContents* web_contents) override
     {return std::string();}
   scoped_refptr<content::DevToolsAgentHost> CreateNewTarget(
     const GURL& url) override {return nullptr;}
