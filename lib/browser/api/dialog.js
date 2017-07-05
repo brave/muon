VideoClient/electron

Add DialogSettings helper struct

lib/browser/api/dialog.js
@@ -121,8 +121,9 @@ module.exports = {
     const wrappedCallback = typeof callback === 'function' ? function (success, result) {
       return callback(success ? result : void 0)
     } : null
-    return binding.showOpenDialog(title, buttonLabel, defaultPath, filters,
-                                  dialogProperties, window, wrappedCallback)
+    const settings = {title, buttonLabel, defaultPath, filters, window}
+    settings.properties = dialogProperties
+    return binding.showOpenDialog(settings, wrappedCallback)
   },
 
   showSaveDialog: function (...args) {
@@ -163,8 +164,8 @@ module.exports = {
     const wrappedCallback = typeof callback === 'function' ? function (success, result) {
       return callback(success ? result : void 0)
     } : null
-    return binding.showSaveDialog(title, buttonLabel, defaultPath, filters,
-                                  window, wrappedCallback)
+    const settings = {title, buttonLabel, defaultPath, filters, window}
+    return binding.showSaveDialog(settings, wrappedCallback)
   },
 
   showMessageBox: function (...args) {

brave/muon

lib/browser/api/dialog.js
@@ -91,7 +91,10 @@ module.exports = {
     wrappedCallback = typeof callback === 'function' ? function (success, result) {
       return callback(success ? result : void 0)
     } : null
-    return binding.showOpenDialog(options.title, options.buttonLabel, options.defaultPath, options.filters, properties, window, wrappedCallback)
+    let settings = options
+    settings.properties = properties
+    settings.window = window
+    return binding.showOpenDialog(settings, wrappedCallback)
   },
 
   showSaveDialog: function (...args) {
@@ -124,7 +127,9 @@ module.exports = {
     wrappedCallback = typeof callback === 'function' ? function (success, result) {
       return callback(success ? result : void 0)
     } : null
-    return binding.showSaveDialog(options.title, options.buttonLabel, options.defaultPath, options.filters, window, wrappedCallback)
+    let settings = options
+    settings.window = window
+    return binding.showSaveDialog(settings, wrappedCallback)
   },
 
   showMessageBox: function (...args) {