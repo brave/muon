c49e1bd40d3700a81e854f5e5d3818573e56bff4
Lorenzo Tilve <ltilve@igalia.com>
2017-07-13 12:33:02 +0200

    Fix call to CheckProtectedPasswordEntry()
    Chromium commit f77d4b0816dfa75bb4e6ebc452ba5f8a7e941c30

--- brave/browser/password_manager/brave_password_manager_client.cc
+++ brave/browser/password_manager/brave_password_manager_client.cc
@@ -350,7 +350,8 @@ void BravePasswordManagerClient::CheckSafeBrowsingReputation(
     const GURL& frame_url) {}
 
 void BravePasswordManagerClient::CheckProtectedPasswordEntry(
-    const std::string& password_saved_domain) {}
+    const std::string& password_saved_domain,
+    bool password_field_exists) {}
 #endif
 
 password_manager::PasswordSyncState
