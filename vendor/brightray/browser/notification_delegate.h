d2a0bcc1e302fa098419f46701c5445662792eeb
Henrique Ferreiro <hferreiro@igalia.com>
2017-08-23 11:46:27 +0200

    Deprecate per notification type delegates
    https://crrev.com/40b10fd5dcb862ce08df81ef5c55c629f64c6394

--- vendor/brightray/browser/notification_delegate.h
+++ vendor/brightray/browser/notification_delegate.h
@@ -2,20 +2,35 @@
 // Use of this source code is governed by the MIT license that can be
 // found in the LICENSE file.
 
-#ifndef BROWSER_NOTIFICATION_DELEGATE_H_
-#define BROWSER_NOTIFICATION_DELEGATE_H_
+#ifndef BRIGHTRAY_BROWSER_NOTIFICATION_DELEGATE_H_
+#define BRIGHTRAY_BROWSER_NOTIFICATION_DELEGATE_H_
 
-#include "content/public/browser/desktop_notification_delegate.h"
+#include <string>
+
+#include "base/macros.h"
 
 namespace brightray {
 
-class NotificationDelegate : public content::DesktopNotificationDelegate {
+class NotificationDelegate {
  public:
+  NotificationDelegate();
+  NotificationDelegate(const std::string& notification_id);
+  virtual ~NotificationDelegate() {}
+
   // The native Notification object is destroyed.
-  virtual void NotificationDestroyed() {}
+  virtual void NotificationDestroyed();
 
   // Failed to send the notification.
   virtual void NotificationFailed() {}
+
+  virtual void NotificationClick();
+  virtual void NotificationClosed();
+  virtual void NotificationDisplayed();
+
+ private:
+  const std::string& notification_id_;
+
+DISALLOW_COPY_AND_ASSIGN(NotificationDelegate);
 };
 
 }  // namespace brightray
