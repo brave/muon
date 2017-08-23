6ff739a5ae8c30ab2e0720ef1583390387eac688
Brian R. Bondy <netzen@gmail.com>
2017-08-23 17:52:58 -0400

    Fix unresolved linking errors on Windows

---
+++ chromium_src/chrome/browser/ui/tabs/tab_utils.cc
@@ -0,0 +1,28 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/tabs/tab_utils.h"
+
+namespace chrome {
+
+bool CanToggleAudioMute(content::WebContents* contents) {
+  NOTREACHED();
+  return false;
+}
+
+TabMutedResult SetTabAudioMuted(content::WebContents* contents,
+                                bool mute,
+                                TabMutedReason reason,
+                                const std::string& extension_id) {
+  NOTREACHED();
+  return TabMutedResult::FAIL_NOT_ENABLED;
+}
+
+bool AreAllTabsMuted(const TabStripModel& tab_strip,
+                     const std::vector<int>& indices) {
+  NOTREACHED();
+  return false;
+}
+
+}  // namespace chrome
