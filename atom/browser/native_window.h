6f2c1da590fd7070866e7dd824dd1584a96aa200
bridiver <github@brianjohnson.cc>
2017-09-05 15:55:05 -0700

    use render frame OnMessage method

--- atom/browser/native_window.h
+++ atom/browser/native_window.h
@@ -265,12 +265,14 @@ class NativeWindow : public base::SupportsUserData,
 
   // Called when the window needs to update its draggable region.
   virtual void UpdateDraggableRegions(
+      content::RenderFrameHost* sender,
       const std::vector<extensions::DraggableRegion>& regions);
 
   // content::WebContentsObserver:
   void BeforeUnloadDialogCancelled() override;
   void DidFirstVisuallyNonEmptyPaint() override;
-  bool OnMessageReceived(const IPC::Message& message) override;
+  bool OnMessageReceived(const IPC::Message& message,
+                         content::RenderFrameHost* sender) override;
 
  private:
   // Schedule a notification unresponsive event.
