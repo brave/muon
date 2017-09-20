374790c4076ee0d914b0a5216bf2c8ec8c381890
Henrique Ferreiro <hferreiro@igalia.com>
2017-09-20 11:55:23 +0200

    The SECURITY_WARNING security level was removed
    https://crrev.com/ec1bba9ca0f2e6d210af6f93948708a95c406ce7

--- atom/browser/api/atom_api_web_contents.cc
+++ atom/browser/api/atom_api_web_contents.cc
@@ -254,9 +254,6 @@ struct Converter<blink::WebSecurityStyle> {
       case blink::kWebSecurityStyleInsecure:
         type = "broken";
         break;
-      case blink::kWebSecurityStyleWarning:
-        type = "warning";
-        break;
       case blink::kWebSecurityStyleSecure:
         type = "secure";
         break;
@@ -276,8 +273,6 @@ struct Converter<blink::WebSecurityStyle> {
       *out = blink::kWebSecurityStyleNeutral;
     } else if (type == "broken") {
       *out = blink::kWebSecurityStyleInsecure;
-    } else if (type == "warning") {
-      *out = blink::kWebSecurityStyleWarning;
     } else if (type == "secure") {
       *out = blink::kWebSecurityStyleSecure;
     } else {
