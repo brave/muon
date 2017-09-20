c890fa86067a9d1381ad5f82c8122d721fe1f4fe
Henrique Ferreiro <hferreiro@igalia.com>
2017-09-20 11:20:08 +0200

    WebDataServiceBase::ShutdownOnUIThread was renamed to ShutdownOnUISequence
    https://crrev.com/197506769e5fe387db4ccb3d92c1131091dff873

--- brave/browser/brave_browser_context.cc
+++ brave/browser/brave_browser_context.cc
@@ -205,9 +205,9 @@ BraveBrowserContext::~BraveBrowserContext() {
   }
 
   if (!IsOffTheRecord() && !HasParentContext()) {
-    autofill_data_->ShutdownOnUIThread();
+    autofill_data_->ShutdownOnUISequence();
 #if defined(OS_WIN)
-    password_data_->ShutdownOnUIThread();
+    password_data_->ShutdownOnUISequence();
 #endif
     web_database_->ShutdownDatabase();
 
