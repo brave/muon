f12a96132e686a16f7e0c7237012cdad4310a034
Henrique Ferreiro <hferreiro@igalia.com>
2017-10-06 12:04:17 +0200

    TODO: DevTools network throttling was moved to //content/common
    https://crrev.com/24f33167852983f3cee26b69639baa36db7b4358

--- chromium_src/chrome/browser/profiles/profile.h
+++ chromium_src/chrome/browser/profiles/profile.h
@@ -161,9 +161,6 @@ class Profile : public atom::AtomBrowserContext {
 
   virtual PrefChangeRegistrar* user_prefs_change_registrar() const = 0;
 
-  virtual DevToolsNetworkControllerHandle*
-  GetDevToolsNetworkControllerHandle() = 0;
-
   virtual bool IsGuestSession() const;
 
   bool IsNewProfile();
