e87a5d33388ae56579dfcda0bfae4640255457d9
Henrique Ferreiro <hferreiro@igalia.com>
2017-10-06 16:03:06 +0200

    The IPC::Sender API is not longer provided by WebContentsObserver
    https://crrev.com/f4357cacb5dc

--- atom/browser/api/atom_api_web_contents.h
+++ atom/browser/api/atom_api_web_contents.h
@@ -533,6 +533,7 @@ class WebContents : public mate::TrackableObject<WebContents>,
                          const base::ListValue& args);
 
   // Called when received a synchronous message from renderer.
+  struct DispatchHelper;
   void OnRendererMessageSync(const base::string16& channel,
                              const base::ListValue& args,
                              IPC::Message* message);
