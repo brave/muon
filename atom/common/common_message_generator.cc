a6efa4bfa6123501769d14d3707c6d7a93ba9d54
Henrique Ferreiro <hferreiro@igalia.com>
2017-10-08 00:06:35 +0200

    ParamTraits::GetSize was removed
    https://crrev.com/f4cec5503d203268fbd8857a8861e56847965a08

--- atom/common/common_message_generator.cc
+++ atom/common/common_message_generator.cc
@@ -14,12 +14,6 @@
 #include "ipc/struct_destructor_macros.h"
 #include "atom/common/common_message_generator.h"
 
-// Generate param traits size methods.
-#include "ipc/param_traits_size_macros.h"
-namespace IPC {
-#include "atom/common/common_message_generator.h"
-}  // namespace IPC
-
 // Generate param traits write methods.
 #include "ipc/param_traits_write_macros.h"
 namespace IPC {
