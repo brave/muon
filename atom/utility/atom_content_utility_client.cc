faec7d4fb858049b5bbabb72508306d0b157c771
Brian Johnson <github@brianjohnson.cc>
2017-10-11 17:05:15 -0700

    Merge pull request #332 from brave/importer-chrome-62
    Overrides service_manager::Service::OnStart() for BraveProfileImportService

--- atom/utility/atom_content_utility_client.cc
+++ atom/utility/atom_content_utility_client.cc
@@ -10,10 +10,10 @@
 #include "base/files/file_path.h"
 #include "base/memory/ref_counted.h"
 #include "base/time/time.h"
+#include "brave/utility/importer/brave_profile_import_service.h"
 #include "chrome/common/importer/profile_import.mojom.h"
 #include "chrome/common/resource_usage_reporter.mojom.h"
 #include "chrome/utility/extensions/extensions_handler.h"
-#include "chrome/utility/importer/profile_import_service.h"
 #include "chrome/utility/utility_message_handler.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/common/service_manager_connection.h"
@@ -134,7 +134,7 @@ void AtomContentUtilityClient::RegisterServices(
     AtomContentUtilityClient::StaticServiceMap* services) {
   service_manager::EmbeddedServiceInfo profile_import_info;
   profile_import_info.factory =
-    base::Bind(&ProfileImportService::CreateService);
+    base::Bind(&BraveProfileImportService::CreateService);
   services->emplace(chrome::mojom::kProfileImportServiceName,
                     profile_import_info);
 }
---
+++ brave/utility/importer/brave_profile_import_service.h
@@ -0,0 +1,27 @@
+// Copyright (c) 2017 The Brave Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+#ifndef BRAVE_UTILITY_IMPORTER_BRAVE_PROFILE_IMPORT_SERVICE_H_
+#define BRAVE_UTILITY_IMPORTER_BRAVE_PROFILE_IMPORT_SERVICE_H_
+
+#include <memory>
+
+#include "chrome/utility/importer/profile_import_service.h"
+
+class BraveProfileImportService : public ProfileImportService {
+ public:
+  BraveProfileImportService();
+  ~BraveProfileImportService() override;
+
+  // Factory method for creating the service.
+  static std::unique_ptr<service_manager::Service> CreateService();
+
+ private:
+  // Lifescycle events that occur after the service has started to spinup.
+  void OnStart() override;
+
+  DISALLOW_COPY_AND_ASSIGN(BraveProfileImportService);
+};
+
+#endif  // BRAVE_UTILITY_IMPORTER_BRAVE_PROFILE_IMPORT_SERVICE_H_
