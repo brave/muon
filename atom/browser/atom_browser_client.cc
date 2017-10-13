722eb38afd10e5d6570802fb145e771a5ee8e24c
Henrique Ferreiro <hferreiro@igalia.com>
2017-10-13 13:12:50 +0200

    Update ContentBrowserClient::AllowCertificateError parameters
    https://crrev.com/9cc660cd8f17

--- atom/browser/atom_browser_client.cc
+++ atom/browser/atom_browser_client.cc
@@ -167,16 +167,14 @@ void AtomBrowserClient::AllowCertificateError(
     const net::SSLInfo& ssl_info,
     const GURL& request_url,
     content::ResourceType resource_type,
-    bool overridable,
     bool strict_enforcement,
     bool expired_previous_decision,
     const base::Callback<void(
         content::CertificateRequestResultType)>& callback) {
   if (delegate_) {
     delegate_->AllowCertificateError(
-        web_contents, cert_error, ssl_info, request_url,
-        resource_type, overridable, strict_enforcement,
-        expired_previous_decision, callback);
+        web_contents, cert_error, ssl_info, request_url, resource_type,
+        strict_enforcement, expired_previous_decision, callback);
   }
 }
 
