412fba0c1f5fdfa1b19d3ef9488c1c616d259546
Lorenzo Tilve <ltilve@igalia.com>
2017-10-17 22:44:50 +0200

    Changes on the FeatureTracker constructor
    https://crrev.com/7a92a20bf6c2

--- chromium_src/chrome/browser/feature_engagement/new_tab/new_tab_tracker.cc
+++ chromium_src/chrome/browser/feature_engagement/new_tab/new_tab_tracker.cc
@@ -4,9 +4,11 @@
 
 #include "chrome/browser/feature_engagement/new_tab/new_tab_tracker.h"
 
+#include "components/feature_engagement/public/feature_constants.h"
+
 namespace {
 
-const int kTwoHoursInMinutes = 120;
+const int kDefaultPromoShowTimeInHours = 2;
 
 }  // namespace
 
@@ -14,17 +16,16 @@ namespace feature_engagement {
 
 NewTabTracker::NewTabTracker(Profile* profile,
                              SessionDurationUpdater* session_duration_updater)
-    : FeatureTracker(profile, session_duration_updater) {}
+    : FeatureTracker(profile,
+                     session_duration_updater,
+                     &kIPHNewTabFeature,
+                     base::TimeDelta::FromHours(kDefaultPromoShowTimeInHours)) {
+}
 
 NewTabTracker::~NewTabTracker() = default;
 
 void NewTabTracker::OnNewTabOpened() {}
 
 void NewTabTracker::OnSessionTimeMet() {}
-
-int NewTabTracker::GetSessionTimeRequiredToShowInMinutes() {
-  NOTREACHED();
-  return kTwoHoursInMinutes;
-}
 
 }  // namespace feature_engagement
