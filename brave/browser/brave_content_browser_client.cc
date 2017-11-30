544644d0c52f42b658982e5e7e097197fdb609bf
Lorenzo Tilve <ltilve@igalia.com>
2017-11-30 11:45:09 +0100

    Remove native_crx_bindings featureSwitch
    https://ccrev.com/a3fe3d605cbb

--- brave/browser/brave_content_browser_client.cc
+++ brave/browser/brave_content_browser_client.cc
@@ -584,7 +584,6 @@ void BraveContentBrowserClient::AppendExtraCommandLineSwitches(
       extensions::switches::kEnableEmbeddedExtensionOptions,
       extensions::switches::kEnableExperimentalExtensionApis,
       extensions::switches::kExtensionsOnChromeURLs,
-      extensions::switches::kNativeCrxBindings,
       extensions::switches::kWhitelistedExtensionID,
       extensions::switches::kYieldBetweenContentScriptRuns,
 #endif
