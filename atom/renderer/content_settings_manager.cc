f534f670c019ef7be9abdc65c3dc264c46fdf1fb
Brian Johnson <github@brianjohnson.cc>
2018-01-04 14:10:14 -0700

    Merge pull request #423 from brave/browser-laptop-issue-12490
    only check origin for remote webframes - https://crbug.com/628759

--- atom/renderer/content_settings_manager.cc
+++ atom/renderer/content_settings_manager.cc
@@ -11,6 +11,8 @@
 #include "components/content_settings/core/common/content_settings_pattern.h"
 #include "content/public/common/url_constants.h"
 #include "content/public/renderer/render_thread.h"
+#include "third_party/WebKit/public/web/WebDocument.h"
+#include "third_party/WebKit/public/web/WebLocalFrame.h"
 #include "url/gurl.h"
 #include "url/url_constants.h"
 
@@ -30,6 +32,19 @@ ContentSettingsManager* ContentSettingsManager::GetInstance() {
   static base::LazyInstance<ContentSettingsManager>::DestructorAtExit manager =
     LAZY_INSTANCE_INITIALIZER;
   return manager.Pointer();
+}
+
+//  static
+GURL ContentSettingsManager::GetOriginOrURL(const blink::WebFrame* frame) {
+  url::Origin top_origin = url::Origin(frame->Top()->GetSecurityOrigin());
+  // The |top_origin| is unique ("null") e.g., for file:// URLs. Use the
+  // document URL as the primary URL in those cases.
+  // TODO(alexmos): This is broken for --site-per-process, since top() can be a
+  // WebRemoteFrame which does not have a document(), and the WebRemoteFrame's
+  // URL is not replicated.  See https://crbug.com/628759.
+  if (top_origin.unique() && frame->Top()->IsWebLocalFrame())
+    return frame->Top()->ToWebLocalFrame()->GetDocument().Url();
+  return top_origin.GetURL();
 }
 
 bool ContentSettingsManager::OnControlMessageReceived(
