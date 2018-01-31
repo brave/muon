a54edb95a3497f7b6aa6d3a86bb5f5e04c17e6ad
Henrique Ferreiro <hferreiro@igalia.com>
2018-01-31 14:17:17 +0100

    Fix chromium_src linking issues

---
+++ chromium_src/chrome/browser/permissions/permission_request_manager.cc
@@ -0,0 +1,11 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/permissions/permission_request_manager.h"
+
+DEFINE_WEB_CONTENTS_USER_DATA_KEY(PermissionRequestManager);
+
+void PermissionRequestManager::AddRequest(PermissionRequest* request) {
+  NOTREACHED();
+}
