45ca1d0bd1c1d7bbecda97c857b970190abf7232
bridiver <github@brianjohnson.cc>
2018-03-01 19:21:13 -0700

    don't ignore callback

--- chromium_src/chrome/browser/safe_browsing/password_reuse_modal_warning_dialog.cc
+++ chromium_src/chrome/browser/safe_browsing/password_reuse_modal_warning_dialog.cc
@@ -1,5 +1,6 @@
 #include "build/build_config.h"
 #include "chrome/browser/safe_browsing/chrome_password_protection_service.h"
+#include "components/safe_browsing/password_protection/password_protection_service.h"
 #include "content/public/browser/web_contents.h"
 
 namespace safe_browsing {
@@ -7,6 +8,8 @@ namespace safe_browsing {
 void ShowPasswordReuseModalWarningDialog(
     content::WebContents* web_contents,
     ChromePasswordProtectionService* service,
-    OnWarningDone done_callback) {}
+    OnWarningDone done_callback) {
+  std::move(done_callback).Run(PasswordProtectionService::IGNORE_WARNING);
+}
 
 }  // namespace safe_browsing
