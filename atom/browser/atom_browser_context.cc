14793c41e0271b0fadbe92c70828e2c36200ffae
Lorenzo Tilve <ltilve@igalia.com>
2018-03-04 13:42:31 +0100

    Remove SequencedWorkerPool
    https://crrev.com/4914ec14f5ba

--- atom/browser/atom_browser_context.cc
+++ atom/browser/atom_browser_context.cc
@@ -24,7 +24,6 @@
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/task_scheduler/post_task.h"
-#include "base/threading/sequenced_worker_pool.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_version.h"
 #include "chrome/common/pref_names.h"
