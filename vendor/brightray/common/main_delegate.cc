7f993ad7f93cc882346f17f1de4af4b6c80149f4
Lorenzo Tilve <ltilve@igalia.com>
2018-03-14 13:11:46 +0100

    Renamed buildflag_headers
    https://crrev.com/9ae8289311df

--- vendor/brightray/common/main_delegate.cc
+++ vendor/brightray/common/main_delegate.cc
@@ -11,7 +11,7 @@
 
 #include "base/command_line.h"
 #include "base/path_service.h"
-#include "components/nacl/common/features.h"
+#include "components/nacl/common/buildflags.h"
 #include "components/nacl/common/nacl_switches.h"
 #include "content/public/common/content_switches.h"
 #include "ui/base/resource/resource_bundle.h"
