5c0b20d8c7fb006f2374d4f4a0db42ece58beeba
Lorenzo Tilve <ltilve@igalia.com>
2018-03-15 14:04:36 +0100

    Mojify spellcheck filter files to spellcheck host files
    https://crrev.com/384fe0f5b0c5
    https://crrev.com/d9085ee1dd4b
    https://crrev.com/16331307f930

--- brave/browser/brave_content_browser_client.cc
+++ brave/browser/brave_content_browser_client.cc
@@ -47,7 +47,6 @@
 #include "components/content_settings/core/common/content_settings.h"
 #include "components/content_settings/core/common/content_settings_types.h"
 #include "components/password_manager/content/browser/content_password_manager_driver_factory.h"
-#include "components/spellcheck/spellcheck_build_features.h"
 #include "components/variations/variations_switches.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/navigation_handle.h"
@@ -86,10 +85,6 @@
 #include "services/ui/public/cpp/gpu/gpu.h"
 #include "ui/aura/mus/window_tree_client.h"
 #include "ui/views/mus/mus_client.h"
-#endif
-
-#if BUILDFLAG(USE_BROWSER_SPELLCHECKER)
-#include "components/spellcheck/browser/spellcheck_message_filter_platform.h"
 #endif
 
 #if BUILDFLAG(ENABLE_PRINT_PREVIEW)
@@ -416,10 +411,6 @@ void BraveContentBrowserClient::RenderProcessWillLaunch(
 
   host->AddFilter(new printing::PrintingMessageFilter(id, profile));
   host->AddFilter(new TtsMessageFilter(host->GetBrowserContext()));
-
-#if BUILDFLAG(USE_BROWSER_SPELLCHECKER)
-  host->AddFilter(new SpellCheckMessageFilterPlatform(id));
-#endif
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
   extensions_part_->RenderProcessWillLaunch(host);
