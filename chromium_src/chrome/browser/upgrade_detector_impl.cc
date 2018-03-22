bc8979e941eef1ca6052200acfca13d281b6d0c1
Henrique Ferreiro <hferreiro@igalia.com>
2018-03-22 13:59:19 +0100

    Fix stub UpgradeDetectorImpl
    https://crrev.com/875403e09166
    https://crrev.com/ded72aba15ca

--- chromium_src/chrome/browser/upgrade_detector_impl.cc
+++ chromium_src/chrome/browser/upgrade_detector_impl.cc
@@ -4,8 +4,23 @@
 
 #include "chrome/browser/upgrade_detector.h"
 
+namespace {
+class UpgradeDetectorImpl : public UpgradeDetector {
+ private:
+  // UpgradeDetector overrides:
+  // Not used in Brave.
+  base::TimeDelta GetHighAnnoyanceLevelDelta() override {
+    return base::TimeDelta();
+  }
+
+  base::TimeTicks GetHighAnnoyanceDeadline() override {
+    return base::TimeTicks();
+  }
+};
+}
+
 // static
 UpgradeDetector* UpgradeDetector::GetInstance() {
-  static auto* const instance = new UpgradeDetector();
+  static auto* const instance = new UpgradeDetectorImpl();
   return instance;
 }
