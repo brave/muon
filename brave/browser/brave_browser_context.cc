brave/muon

commit 25c0a4f2fc1e524eff41bf9132354c23e37270b9

use WebDataServiceWrapper auditors @darkdh @hferreiro

brave/browser/brave_browser_context.cc
@@ -42,6 +42,7 @@
 #include "components/sync_preferences/pref_service_syncable_factory.h"
 #include "components/user_prefs/user_prefs.h"
 #include "components/zoom/zoom_event_manager.h"
+#include "components/webdata_services/web_data_service_wrapper.h"
 #include "components/webdata/common/webdata_constants.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/notification_source.h"
@@ -100,6 +101,15 @@ void NotifyOTRProfileDestroyedOnIOThread(void* original_profile,
       ->OnOTRBrowserContextDestroyed(original_profile, otr_profile);
 }
 #endif
+
+void DummyFlare(syncer::ModelType type) {}
+
+void ProfileErrorCallback(WebDataServiceWrapper::ErrorType error_type,
+                          sql::InitStatus status,
+                          const std::string& diagnostics) {
+  // TODO(bridiver) - this should be reported back to JS
+  LOG(ERROR) << "Error initializing web data service " << diagnostics;
+}
 
 // WATCH(bridiver) - chrome/browser/profiles/profile_impl.cc
 // Converts the kSessionExitedCleanly pref to the corresponding EXIT_TYPE.
@@ -212,11 +222,7 @@ BraveBrowserContext::~BraveBrowserContext() {
   }
 
   if (!IsOffTheRecord() && !HasParentContext()) {
-    autofill_data_->ShutdownOnUISequence();
-#if defined(OS_WIN)
-    password_data_->ShutdownOnUISequence();
-#endif
-    web_database_->ShutdownDatabase();
+    web_database_wrapper_->Shutdown();
 
     bool prefs_loaded = user_prefs_->GetInitializationStatus() !=
         PrefService::INITIALIZATION_STATUS_WAITING;
@@ -524,36 +530,12 @@ void BraveBrowserContext::OnPrefsLoaded(bool success) {
           base::FilePath());
     }
 
-    // Initialize autofill db
-    base::FilePath webDataPath = GetPath().Append(kWebDataFilename);
-
-    auto ui_task_runner =
-        BrowserThread::GetTaskRunnerForThread(BrowserThread::UI);
-    auto db_task_runner = base::CreateSingleThreadTaskRunnerWithTraits(
-        {base::MayBlock(), base::TaskPriority::USER_VISIBLE,
-         base::TaskShutdownBehavior::BLOCK_SHUTDOWN});
-
-    CHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
-    web_database_ =
-       new WebDatabaseService(webDataPath, ui_task_runner, db_task_runner);
-    web_database_->AddTable(base::WrapUnique(new autofill::AutofillTable));
-    web_database_->AddTable(base::WrapUnique(new LoginsTable));
-    web_database_->LoadDatabase();
-
-    autofill_data_ = new autofill::AutofillWebDataService(
-        web_database_,
-        ui_task_runner,
-        db_task_runner,
-        base::Bind(&DatabaseErrorCallback));
-    autofill_data_->Init();
-
-#if defined(OS_WIN)
-    password_data_ = new PasswordWebDataService(
-        web_database_,
-        ui_task_runner,
-        base::Bind(&PasswordErrorCallback));
-    password_data_->Init();
-#endif
+    const base::FilePath& profile_path = GetPath();
+    web_database_wrapper_.reset(new WebDataServiceWrapper(
+        profile_path, g_browser_process->GetApplicationLocale(),
+        BrowserThread::GetTaskRunnerForThread(BrowserThread::UI),
+        base::Bind(&DummyFlare),
+        base::BindRepeating(&ProfileErrorCallback)));
   }
 
   user_prefs_registrar_->Init(user_prefs_.get());
@@ -591,13 +573,13 @@ BraveBrowserContext::CreateZoomLevelDelegate(
 
 scoped_refptr<autofill::AutofillWebDataService>
 BraveBrowserContext::GetAutofillWebdataService() {
-  return original_context()->autofill_data_;
+  return original_context()->web_database_wrapper_->GetAutofillWebData();
 }
 
 #if defined(OS_WIN)
 scoped_refptr<PasswordWebDataService>
 BraveBrowserContext::GetPasswordWebdataService() {
-  return original_context()->password_data_;
+  return original_context()->web_database_wrapper_->GetPasswordWebData();
 }
 #endif
 

atom/browser/api/atom_api_autofill.h
@@ -20,6 +20,7 @@
 
 namespace autofill {
 class AutofillProfile;
+class CreditCard;
 class PersonalDataManager;
 }
 

brave/browser/BUILD.gn
@@ -59,6 +59,7 @@ source_set("browser") {
     "//components/ukm/content",
     "//components/visitedlink/browser",
     "//components/visitedlink/common",
+    "//components/webdata_services",
     "//content/public/common:service_names",
     "//device/geolocation",
     "//device/usb/mojo",

brave/browser/brave_browser_context.h
@@ -14,14 +14,11 @@
 #include "chrome/browser/custom_handlers/protocol_handler_registry.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/zoom/chrome_zoom_level_prefs.h"
-#include "components/autofill/core/browser/webdata/autofill_webdata_service.h"
-#if defined(OS_WIN)
-#include "components/password_manager/core/browser/webdata/password_web_data_service_win.h"
-#endif
 #include "components/prefs/overlay_user_pref_store.h"
 #include "components/webdata/common/web_database_service.h"
 
 class PrefChangeRegistrar;
+class WebDataServiceWrapper;
 
 namespace extensions {
 class InfoMap;
@@ -154,11 +151,7 @@ class BraveBrowserContext : public Profile {
   const std::string partition_;
   std::unique_ptr<base::WaitableEvent> ready_;
 
-  scoped_refptr<autofill::AutofillWebDataService> autofill_data_;
-#if defined(OS_WIN)
-  scoped_refptr<PasswordWebDataService> password_data_;
-#endif
-  scoped_refptr<WebDatabaseService> web_database_;
+  std::unique_ptr<WebDataServiceWrapper> web_database_wrapper_;
   std::unique_ptr<ProtocolHandlerRegistry::JobInterceptorFactory>
       protocol_handler_interceptor_;
 

commit 3bdb83115b936c7898a51d8d1b4d73cdb255a448

Fix windows build error of incomplete `PasswordWebDataService` introduced by 25c0a4f2fc1e524eff41bf9132354c23e37270b9
Auditor: @bridiver

brave/browser/brave_browser_context.cc
@@ -75,6 +75,10 @@
 #include "chrome/browser/plugins/plugin_info_host_impl.h"
 #endif
 
+#if defined(OS_WIN)
+#include "components/password_manager/core/browser/webdata/password_web_data_service_win.h"
+#endif
+
 using content::BrowserThread;
 using content::HostZoomMap;
 