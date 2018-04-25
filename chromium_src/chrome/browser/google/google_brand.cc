d6d3d4bb701670774df71b8e85b1055cada97a1d
Henrique Ferreiro <hferreiro@igalia.com>
2018-04-25 13:52:19 +0200

    Rework UpgradeDetectorImpl in chromium_src/
    Use the upstream version and add stubs for
    chrome/browser/google/google_brand.cc. It fixes the following linking
    error:
    
      error: allocating an object of abstract class type '(anonymous
      namespace)::UpgradeDetectorImpl'
      static auto* const instance = new UpgradeDetectorImpl();
                                        ^

---
+++ chromium_src/chrome/browser/google/google_brand.cc
@@ -0,0 +1,17 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/google/google_brand.h"
+
+namespace google_brand {
+
+bool GetBrand(std::string* brand) {
+  return false;
+}
+
+bool IsOrganic(const std::string& brand) {
+  return false;
+}
+
+}
