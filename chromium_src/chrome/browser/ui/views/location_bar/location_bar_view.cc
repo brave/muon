4a59e8fe747274fb0b9fab227574b4eb8219a023
Henrique Ferreiro <hferreiro@igalia.com>
2018-04-30 12:12:07 +0200

    Stub some LocationBarView static methods
    Fixes this linking errors:
    
      error: undefined symbol: LocationBarView::GetAvailableTextHeight()
      >>> referenced by chrome_typography.cc:62
      (../../chrome/browser/ui/views/harmony/chrome_typography.cc:62)
      >>> obj/electron/chromium_src/browser/chrome_typography.o:
        (ApplyCommonFontStyles(int, int, int*, gfx::Font::Weight*))
    
      error: undefined symbol: LocationBarView::
        GetAvailableDecorationTextHeight()
      >>> referenced by chrome_typography.cc:78
      (../../chrome/browser/ui/views/harmony/chrome_typography.cc:78)
      >>> obj/electron/chromium_src/browser/chrome_typography.o:
        (ApplyCommonFontStyles(int, int, int*, gfx::Font::Weight*))

---
+++ chromium_src/chrome/browser/ui/views/location_bar/location_bar_view.cc
@@ -0,0 +1,13 @@
+#include "chrome/browser/ui/views/location_bar/location_bar_view.h"
+
+// static
+int LocationBarView::GetAvailableTextHeight() {
+  NOTREACHED();
+  return 0;
+}
+
+// static
+int LocationBarView::GetAvailableDecorationTextHeight() {
+  NOTREACHED();
+  return 0;
+}
