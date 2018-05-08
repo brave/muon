9b7ab85703a2b282098455007949b207cc2b5750
Anthony Tseng <darkdh@gmail.com>
2018-05-08 11:45:00 -0700

    Use TorLauncherFactory to launch tor when browser context init

---
+++ brave/browser/tor/tor_launcher_factory.h
@@ -0,0 +1,37 @@
+// Copyright 2018 The Brave Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BRAVE_BROWSER_TOR_TOR_LAUNCHER_FACTORY_H_
+#define BRAVE_BROWSER_TOR_TOR_LAUNCHER_FACTORY_H_
+
+#include <string>
+
+#include "brave/common/tor/tor.mojom.h"
+
+namespace brave {
+
+class TorLauncherFactory {
+ public:
+  TorLauncherFactory(const base::FilePath::StringType& path,
+                      const std::string& proxy);
+  ~TorLauncherFactory();
+
+  void LaunchTorProcess();
+ private:
+  void LaunchInLauncherThread();
+
+  void OnTorLauncherCrashed();
+  void OnTorCrashed(int64_t pid);
+  void OnTorLaunched(bool result);
+
+  tor::mojom::TorLauncherPtr tor_launcher_;
+
+  base::FilePath::StringType path_;
+  std::string host_;
+  std::string port_;
+};
+
+}  // namespace brave
+
+#endif  // BRAVE_BROWSER_TOR_TOR_LAUNCHER_FACTORY_H_
