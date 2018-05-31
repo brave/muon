8e43681b362911accefe9dd3c015fe27a4706f85
Henrique Ferreiro <hferreiro@igalia.com>
2018-05-31 12:27:30 +0200

    chromium_src: rename resource_coordinator_web_contents_observer.* => tab_helper.*
    https://crrev.com/c502833d6d71

--- atom/browser/api/atom_api_web_contents.cc
+++ atom/browser/api/atom_api_web_contents.cc
@@ -61,7 +61,7 @@
 #include "chrome/browser/printing/print_preview_message_handler.h"
 #include "chrome/browser/printing/print_view_manager.h"
 #include "chrome/browser/printing/print_view_manager_common.h"
-#include "chrome/browser/resource_coordinator/resource_coordinator_web_contents_observer.h"
+#include "chrome/browser/resource_coordinator/tab_helper.h"
 #include "chrome/browser/resource_coordinator/tab_manager.h"
 #include "chrome/browser/safe_browsing/safe_browsing_service.h"
 #include "chrome/browser/spellchecker/spellcheck_factory.h"
@@ -608,8 +608,8 @@ void WebContents::CompleteInit(v8::Isolate* isolate,
         this);
     }
 
-    if (ResourceCoordinatorWebContentsObserver::IsEnabled())
-      ResourceCoordinatorWebContentsObserver::CreateForWebContents(
+    if (resource_coordinator::ResourceCoordinatorTabHelper::IsEnabled())
+      resource_coordinator::ResourceCoordinatorTabHelper::CreateForWebContents(
           web_contents);
   }
 
