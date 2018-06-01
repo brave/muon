c461b9b045f99c6bca495da320049903c20ac4ad
Henrique Ferreiro <hferreiro@igalia.com>
2018-06-01 13:38:57 +0200

    Implement missing BraveCredentialsFilter::ShouldSavePasswordHash()
    https://crrev.com/f470d5329889

--- brave/browser/password_manager/brave_credentials_filter.h
+++ brave/browser/password_manager/brave_credentials_filter.h
@@ -24,6 +24,8 @@ class BraveCredentialsFilter : public CredentialsFilter {
       std::vector<std::unique_ptr<autofill::PasswordForm>> results)
       const override;
   bool ShouldSave(const autofill::PasswordForm& form) const override;
+  bool ShouldSavePasswordHash(
+      const autofill::PasswordForm& form) const override;
   void ReportFormLoginSuccess(
       const PasswordFormManager& form_manager) const override;
 
