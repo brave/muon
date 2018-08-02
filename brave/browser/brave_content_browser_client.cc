2ac3d06f4ec1f0ded2129447cb899af0a284ba58
Brian Johnson <github@brianjohnson.cc>
2018-08-02 10:09:23 -0700

    Merge pull request #637 from brave/app_locale
    Use `BrowserProcessImpl::GetApplicationLocale` for `BraveContentBrowserClient::GetApplicationLocale`

--- brave/browser/brave_content_browser_client.cc
+++ brave/browser/brave_content_browser_client.cc
@@ -584,7 +584,7 @@ std::string BraveContentBrowserClient::GetApplicationLocale() {
   if (BrowserThread::CurrentlyOn(BrowserThread::IO)) {
     return io_thread_application_locale.Get();
   } else {
-    return extensions_part_->GetApplicationLocale();
+    return g_browser_process->GetApplicationLocale();
   }
 }
 
