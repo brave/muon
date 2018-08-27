1b688720e33d50336f70a3066c554aff92e9b87b
Anthony Tseng <darkdh@gmail.com>
2018-08-27 14:46:36 -0700

    Make sure seatbelt server initialize sandbox upstream commits: `git show 4c69297c5da0d7332ee3a24a985fcc0b89ce3cc1^..37128e1b486709b4d27cfd5049ad8db404b8b3d0 chrome/app/chrome_exe_main_mac.cc`
    Auditors: @bridiver, @hferreiro, @ltilve

---
+++ chromium_src/chrome/app/chrome_exe_main_mac.cc
@@ -0,0 +1,106 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// The entry point for all Mac Chromium processes, including the outer app
+// bundle (browser) and helper app (renderer, plugin, and friends).
+
+#include <dlfcn.h>
+#include <errno.h>
+#include <libgen.h>
+#include <mach-o/dyld.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <memory>
+
+#include "chrome/common/chrome_version.h"
+
+#if defined(HELPER_EXECUTABLE)
+#include "sandbox/mac/seatbelt_exec.h"
+#endif  // defined(HELPER_EXECUTABLE)
+
+namespace {
+
+typedef int (*ChromeMainPtr)(int, char**);
+
+}  // namespace
+
+__attribute__((visibility("default"))) int main(int argc, char* argv[]) {
+  uint32_t exec_path_size = 0;
+  int rv = _NSGetExecutablePath(NULL, &exec_path_size);
+  if (rv != -1) {
+    fprintf(stderr, "_NSGetExecutablePath: get length failed\n");
+    abort();
+  }
+
+  std::unique_ptr<char[]> exec_path(new char[exec_path_size]);
+  rv = _NSGetExecutablePath(exec_path.get(), &exec_path_size);
+  if (rv != 0) {
+    fprintf(stderr, "_NSGetExecutablePath: get path failed\n");
+    abort();
+  }
+
+#if defined(HELPER_EXECUTABLE)
+  sandbox::SeatbeltExecServer::CreateFromArgumentsResult seatbelt =
+      sandbox::SeatbeltExecServer::CreateFromArguments(exec_path.get(), argc,
+                                                       argv);
+  if (seatbelt.sandbox_required) {
+    if (!seatbelt.server) {
+      fprintf(stderr, "Failed to create seatbelt sandbox server.\n");
+      abort();
+    }
+    if (!seatbelt.server->InitializeSandbox()) {
+      fprintf(stderr, "Failed to initialize sandbox.\n");
+      abort();
+    }
+  }
+
+  const char rel_path[] =
+      "../../../" PRODUCT_FULLNAME_STRING
+      " Framework.framework/" PRODUCT_FULLNAME_STRING " Framework";
+#else
+  const char rel_path[] =
+      "../Frameworks/" PRODUCT_FULLNAME_STRING
+      " Framework.framework/" PRODUCT_FULLNAME_STRING " Framework";
+#endif  // defined(HELPER_EXECUTABLE)
+
+  // Slice off the last part of the main executable path, and append the
+  // version framework information.
+  const char* parent_dir = dirname(exec_path.get());
+  if (!parent_dir) {
+    fprintf(stderr, "dirname %s: %s\n", exec_path.get(), strerror(errno));
+    abort();
+  }
+
+  const size_t parent_dir_len = strlen(parent_dir);
+  const size_t rel_path_len = strlen(rel_path);
+  // 2 accounts for a trailing NUL byte and the '/' in the middle of the paths.
+  const size_t framework_path_size = parent_dir_len + rel_path_len + 2;
+  std::unique_ptr<char[]> framework_path(new char[framework_path_size]);
+  snprintf(framework_path.get(), framework_path_size, "%s/%s", parent_dir,
+           rel_path);
+
+  void* library =
+      dlopen(framework_path.get(), RTLD_LAZY | RTLD_GLOBAL | RTLD_FIRST);
+  if (!library) {
+    fprintf(stderr, "dlopen %s: %s\n", framework_path.get(), dlerror());
+    abort();
+  }
+
+  const ChromeMainPtr chrome_main =
+      reinterpret_cast<ChromeMainPtr>(dlsym(library, "ChromeMain"));
+  if (!chrome_main) {
+    fprintf(stderr, "dlsym ChromeMain: %s\n", dlerror());
+    abort();
+  }
+  rv = chrome_main(argc, argv);
+
+  // exit, don't return from main, to avoid the apparent removal of main from
+  // stack backtraces under tail call optimization.
+  exit(rv);
+}
