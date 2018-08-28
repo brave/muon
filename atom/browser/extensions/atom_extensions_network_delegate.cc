brave/muon

commit c5188ecf8530b11b7ebea1594dd6ccd875b312c7

TODO: Use net::CompletionOnceCallback in place of net::CompletionCallback
http://crbug.com/807724

atom/browser/extensions/atom_extensions_network_delegate.cc
@@ -4,6 +4,8 @@
 
 #include "atom/browser/extensions/atom_extensions_network_delegate.h"
 
+#include <utility>
+
 #include "base/stl_util.h"
 #include "chrome/browser/extensions/event_router_forwarder.h"
 #include "chrome/browser/net/chrome_extensions_network_delegate.h"
@@ -48,11 +50,11 @@ void AtomExtensionsNetworkDelegate::RunCallback(
 
     if (result != net::ERR_IO_PENDING) {
       // nothing ran the original callback
-      callbacks_[request_id].Run(net::OK);
+      std::move(callbacks_[request_id]).Run(net::OK);
     }
   } else {
     // nothing ran the original callback
-    callbacks_[request_id].Run(previous_result);
+    std::move(callbacks_[request_id]).Run(previous_result);
   }
 
   // make sure this don't run again
@@ -63,16 +65,16 @@ int AtomExtensionsNetworkDelegate::OnBeforeURLRequestInternal(
     net::URLRequest* request,
     GURL* new_url) {
   return atom::AtomNetworkDelegate::OnBeforeURLRequest(
-      request, callbacks_[request->identifier()], new_url);
+      request, std::move(callbacks_[request->identifier()]), new_url);
 }
 
 int AtomExtensionsNetworkDelegate::OnBeforeURLRequest(
     net::URLRequest* request,
-    const net::CompletionCallback& callback,
+    net::CompletionOnceCallback callback,
     GURL* new_url) {
   extensions_delegate_->ForwardStartRequestStatus(request);
 
-  callbacks_[request->identifier()] = callback;
+  callbacks_[request->identifier()] = std::move(callback);
 
   base::Callback<int(void)> internal_callback = base::Bind(
           &AtomExtensionsNetworkDelegate::OnBeforeURLRequestInternal,
@@ -85,8 +87,8 @@ int AtomExtensionsNetworkDelegate::OnBeforeURLRequest(
                                 internal_callback,
                                 request->identifier());
 
-  int result = extensions_delegate_->OnBeforeURLRequest(
-      request, wrapped_cb, new_url);
+  int result = extensions_delegate_->NotifyBeforeURLRequest(
+      request, std::move(wrapped_cb), new_url);
 
   if (result == net::OK)
     return internal_callback.Run();
@@ -99,16 +101,16 @@ int AtomExtensionsNetworkDelegate::OnBeforeStartTransactionInternal(
     net::HttpRequestHeaders* headers) {
   return atom::AtomNetworkDelegate::OnBeforeStartTransaction(
     request,
-    callbacks_[request->identifier()],
+    std::move(callbacks_[request->identifier()]),
     headers);
 }
 
 int AtomExtensionsNetworkDelegate::OnBeforeStartTransaction(
     net::URLRequest* request,
-    const net::CompletionCallback& callback,
+    net::CompletionOnceCallback callback,
     net::HttpRequestHeaders* headers) {
 
-  callbacks_[request->identifier()] = callback;
+  callbacks_[request->identifier()] = std::move(callback);
 
   base::Callback<int(void)> internal_callback = base::Bind(
           &AtomExtensionsNetworkDelegate::OnBeforeStartTransactionInternal,
@@ -121,8 +123,8 @@ int AtomExtensionsNetworkDelegate::OnBeforeStartTransaction(
                                 internal_callback,
                                 request->identifier());
 
-  int result = extensions_delegate_->OnBeforeStartTransaction(
-      request, wrapped_cb, headers);
+  int result = extensions_delegate_->NotifyBeforeStartTransaction(
+      request, std::move(callback), headers);
 
   if (result == net::ERR_IO_PENDING)
     return result;
@@ -134,7 +136,7 @@ void AtomExtensionsNetworkDelegate::OnStartTransaction(
     net::URLRequest* request,
     const net::HttpRequestHeaders& headers) {
   atom::AtomNetworkDelegate::OnStartTransaction(request, headers);
-  extensions_delegate_->OnStartTransaction(request, headers);
+  extensions_delegate_->NotifyStartTransaction(request, headers);
 }
 
 int AtomExtensionsNetworkDelegate::OnHeadersReceivedInternal(
@@ -143,21 +145,19 @@ int AtomExtensionsNetworkDelegate::OnHeadersReceivedInternal(
     scoped_refptr<net::HttpResponseHeaders>* override_response_headers,
     GURL* allowed_unsafe_redirect_url) {
   return atom::AtomNetworkDelegate::OnHeadersReceived(
-                                          request,
-                                          callbacks_[request->identifier()],
-                                          original_response_headers,
-                                          override_response_headers,
-                                          allowed_unsafe_redirect_url);
+      request, std::move(callbacks_[request->identifier()]),
+      original_response_headers, override_response_headers,
+      allowed_unsafe_redirect_url);
 }
 
 int AtomExtensionsNetworkDelegate::OnHeadersReceived(
     net::URLRequest* request,
-    const net::CompletionCallback& callback,
+    net::CompletionOnceCallback callback,
     const net::HttpResponseHeaders* original_response_headers,
     scoped_refptr<net::HttpResponseHeaders>* override_response_headers,
     GURL* allowed_unsafe_redirect_url) {
 
-  callbacks_[request->identifier()] = callback;
+  callbacks_[request->identifier()] = std::move(callback);
 
   base::Callback<int(void)> internal_callback =
       base::Bind(&AtomExtensionsNetworkDelegate::OnHeadersReceivedInternal,
@@ -172,12 +172,9 @@ int AtomExtensionsNetworkDelegate::OnHeadersReceived(
                                 internal_callback,
                                 request->identifier());
 
-  int result = extensions_delegate_->OnHeadersReceived(
-      request,
-      wrapped_cb,
-      original_response_headers,
-      override_response_headers,
-      allowed_unsafe_redirect_url);
+  int result = extensions_delegate_->NotifyHeadersReceived(
+      request, std::move(wrapped_cb), original_response_headers,
+      override_response_headers, allowed_unsafe_redirect_url);
 
   if (result == net::OK)
     return internal_callback.Run();
@@ -189,14 +186,14 @@ void AtomExtensionsNetworkDelegate::OnBeforeRedirect(
     net::URLRequest* request,
     const GURL& new_location) {
   atom::AtomNetworkDelegate::OnBeforeRedirect(request, new_location);
-  extensions_delegate_->OnBeforeRedirect(request, new_location);
+  extensions_delegate_->NotifyBeforeRedirect(request, new_location);
 }
 
 void AtomExtensionsNetworkDelegate::OnResponseStarted(
     net::URLRequest* request,
     int net_error) {
   atom::AtomNetworkDelegate::OnResponseStarted(request, net_error);
-  extensions_delegate_->OnResponseStarted(request, net_error);
+  extensions_delegate_->NotifyResponseStarted(request, net_error);
 }
 
 void AtomExtensionsNetworkDelegate::OnCompleted(
@@ -206,7 +203,7 @@ void AtomExtensionsNetworkDelegate::OnCompleted(
   callbacks_.erase(request->identifier());
   atom::AtomNetworkDelegate::OnCompleted(request, started, net_error);
 
-  extensions_delegate_->OnCompleted(request, started, net_error);
+  extensions_delegate_->NotifyCompleted(request, started, net_error);
   extensions_delegate_->ForwardProxyErrors(request, net_error);
   extensions_delegate_->ForwardDoneRequestStatus(request);
 }
@@ -215,23 +212,23 @@ void AtomExtensionsNetworkDelegate::OnURLRequestDestroyed(
     net::URLRequest* request) {
   callbacks_.erase(request->identifier());
   atom::AtomNetworkDelegate::OnURLRequestDestroyed(request);
-  extensions_delegate_->OnURLRequestDestroyed(request);
+  extensions_delegate_->NotifyURLRequestDestroyed(request);
 }
 
 void AtomExtensionsNetworkDelegate::OnPACScriptError(
     int line_number,
     const base::string16& error) {
-  extensions_delegate_->OnPACScriptError(line_number, error);
+  extensions_delegate_->NotifyPACScriptError(line_number, error);
 }
 
 net::NetworkDelegate::AuthRequiredResponse
 AtomExtensionsNetworkDelegate::OnAuthRequired(
     net::URLRequest* request,
     const net::AuthChallengeInfo& auth_info,
-    const AuthCallback& callback,
+    AuthCallback callback,
     net::AuthCredentials* credentials) {
-  return extensions_delegate_->OnAuthRequired(
-      request, auth_info, callback, credentials);
+  return extensions_delegate_->NotifyAuthRequired(
+      request, auth_info, std::move(callback), credentials);
 }
 
 }  // namespace extensions

atom/browser/api/atom_api_session.cc
@@ -309,10 +309,10 @@ void DoCacheActionInIO(
 
 void SetCertVerifyProcInIO(
     const scoped_refptr<net::URLRequestContextGetter>& context_getter,
-    const AtomCertVerifier::VerifyProc& proc) {
+    AtomCertVerifier::VerifyProc proc) {
   auto request_context = context_getter->GetURLRequestContext();
   static_cast<AtomCertVerifier*>(request_context->cert_verifier())->
-      SetVerifyProc(proc);
+      SetVerifyProc(std::move(proc));
 }
 
 void ClearHostResolverCacheInIO(
@@ -492,15 +492,17 @@ void Session::SetDownloadPath(const base::FilePath& path) {
 void Session::SetCertVerifyProc(v8::Local<v8::Value> val,
                                 mate::Arguments* args) {
   AtomCertVerifier::VerifyProc proc;
-  if (!(val->IsNull() || mate::ConvertFromV8(args->isolate(), val, &proc))) {
-    args->ThrowError("Must pass null or function");
-    return;
-  }
+  // TODO(hferreiro)
+  // if (!(val->IsNull() ||
+  //      mate::ConvertFromV8(args->isolate(), val, std::move(proc)))) {
+  //  args->ThrowError("Must pass null or function");
+  //  return;
+  //}
 
-  BrowserThread::PostTask(BrowserThread::IO, FROM_HERE,
-      base::Bind(&SetCertVerifyProcInIO,
-                 request_context_getter_,
-                 proc));
+  BrowserThread::PostTask(
+      BrowserThread::IO, FROM_HERE,
+      base::BindOnce(&SetCertVerifyProcInIO, request_context_getter_,
+                     std::move(proc)));
 }
 
 void Session::SetPermissionRequestHandler(v8::Local<v8::Value> val,

atom/browser/atom_browser_context.cc
@@ -30,6 +30,7 @@
 #include "components/prefs/pref_registry_simple.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/common/url_constants.h"
+#include "net/base/completion_once_callback.h"
 #include "net/ftp/ftp_network_layer.h"
 #include "net/net_buildflags.h"
 #include "net/url_request/data_protocol_handler.h"
@@ -50,7 +51,7 @@ namespace {
 class NoCacheBackend : public net::HttpCache::BackendFactory {
   int CreateBackend(net::NetLog* net_log,
                     std::unique_ptr<disk_cache::Backend>* backend,
-                    const net::CompletionCallback& callback) override {
+                    net::CompletionOnceCallback callback) override {
     return net::ERR_FAILED;
   }
 };

atom/browser/extensions/atom_extensions_browser_client.cc
@@ -178,7 +178,7 @@ class URLRequestResourceBundleJob : public net::URLRequestSimpleJob {
                       std::string* charset,
                       scoped_refptr<base::RefCountedMemory> data,
                       std::string* read_mime_type,
-                      const net::CompletionCallback& callback,
+                      net::CompletionOnceCallback callback,
                       bool read_result) {
     *out_mime_type = *read_mime_type;
     if (base::StartsWith(*read_mime_type, "text/",
@@ -190,7 +190,7 @@ class URLRequestResourceBundleJob : public net::URLRequestSimpleJob {
       *charset = "utf-8";
     }
     int result = read_result ? net::OK : net::ERR_INVALID_URL;
-    callback.Run(result);
+    std::move(callback).Run(result);
   }
 
   // We need the filename of the resource to determine the mime type.

atom/browser/extensions/atom_extensions_network_delegate.h
@@ -11,6 +11,7 @@
 #include "atom/browser/extensions/atom_extension_system.h"
 #include "atom/browser/extensions/atom_extension_system_factory.h"
 #include "atom/browser/net/atom_network_delegate.h"
+#include "net/base/completion_once_callback.h"
 
 class ChromeExtensionsNetworkDelegate;
 class Profile;
@@ -32,10 +33,10 @@ class AtomExtensionsNetworkDelegate : public atom::AtomNetworkDelegate {
 
  protected:
   int OnBeforeURLRequest(net::URLRequest* request,
-                         const net::CompletionCallback& callback,
+                         net::CompletionOnceCallback callback,
                          GURL* new_url) override;
   int OnBeforeStartTransaction(net::URLRequest* request,
-                               const net::CompletionCallback& callback,
+                               net::CompletionOnceCallback callback,
                                net::HttpRequestHeaders* headers) override;
   void OnBeforeRedirect(net::URLRequest* request,
                         const GURL& new_location) override;
@@ -57,7 +58,7 @@ class AtomExtensionsNetworkDelegate : public atom::AtomNetworkDelegate {
     GURL* allowed_unsafe_redirect_url);
   int OnHeadersReceived(
       net::URLRequest* request,
-      const net::CompletionCallback& callback,
+      net::CompletionOnceCallback callback,
       const net::HttpResponseHeaders* original_response_headers,
       scoped_refptr<net::HttpResponseHeaders>* override_response_headers,
       GURL* allowed_unsafe_redirect_url) override;
@@ -70,14 +71,14 @@ class AtomExtensionsNetworkDelegate : public atom::AtomNetworkDelegate {
   net::NetworkDelegate::AuthRequiredResponse OnAuthRequired(
       net::URLRequest* request,
       const net::AuthChallengeInfo& auth_info,
-      const AuthCallback& callback,
+      AuthCallback callback,
       net::AuthCredentials* credentials) override;
   void RunCallback(base::Callback<int(void)> internal_callback,
                     const uint64_t request_id,
                     int previous_result);
 
   Profile* profile_;
-  std::map<uint64_t, net::CompletionCallback> callbacks_;
+  std::map<uint64_t, net::CompletionOnceCallback> callbacks_;
   std::unique_ptr<ChromeExtensionsNetworkDelegate> extensions_delegate_;
   extensions::EventRouterForwarder* extension_event_router_forwarder_;
 

atom/browser/net/atom_cert_verifier.cc
@@ -4,6 +4,8 @@
 
 #include "atom/browser/net/atom_cert_verifier.h"
 
+#include <utility>
+
 #include "atom/browser/browser.h"
 #include "atom/common/native_mate_converters/net_converter.h"
 #include "content/public/browser/browser_thread.h"
@@ -19,11 +21,11 @@ namespace {
 
 void OnResult(
     net::CertVerifyResult* verify_result,
-    const net::CompletionCallback& callback,
+    net::CompletionOnceCallback callback,
     bool result) {
   BrowserThread::PostTask(
       BrowserThread::IO, FROM_HERE,
-      base::Bind(callback, result ? net::OK : net::ERR_FAILED));
+      base::BindOnce(std::move(callback), result ? net::OK : net::ERR_FAILED));
 }
 
 }  // namespace
@@ -35,27 +37,28 @@ AtomCertVerifier::AtomCertVerifier()
 AtomCertVerifier::~AtomCertVerifier() {
 }
 
-void AtomCertVerifier::SetVerifyProc(const VerifyProc& proc) {
-  verify_proc_ = proc;
+void AtomCertVerifier::SetVerifyProc(VerifyProc proc) {
+  verify_proc_ = std::move(proc);
 }
 
 int AtomCertVerifier::Verify(
     const RequestParams& params,
     net::CRLSet* crl_set,
     net::CertVerifyResult* verify_result,
-    const net::CompletionCallback& callback,
+    net::CompletionOnceCallback callback,
     std::unique_ptr<Request>* out_req,
     const net::NetLogWithSource& net_log) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
 
-  if (verify_proc_.is_null())
+  if (!verify_proc_)
     return default_cert_verifier_->Verify(
-        params, crl_set, verify_result, callback, out_req, net_log);
+        params, crl_set, verify_result, std::move(callback), out_req, net_log);
 
   BrowserThread::PostTask(
       BrowserThread::UI, FROM_HERE,
-      base::Bind(verify_proc_, params.hostname(), params.certificate(),
-                 base::Bind(OnResult, verify_result, callback)));
+      base::BindOnce(
+          std::move(verify_proc_), params.hostname(), params.certificate(),
+          base::BindOnce(OnResult, verify_result, std::move(callback))));
   return net::ERR_IO_PENDING;
 }
 

atom/browser/net/atom_cert_verifier.h
@@ -8,6 +8,7 @@
 #include <memory>
 #include <string>
 
+#include "net/base/completion_once_callback.h"
 #include "net/cert/cert_verifier.h"
 
 namespace atom {
@@ -18,18 +19,18 @@ class AtomCertVerifier : public net::CertVerifier {
   virtual ~AtomCertVerifier();
 
   using VerifyProc =
-      base::Callback<void(const std::string& hostname,
-                          scoped_refptr<net::X509Certificate>,
-                          const base::Callback<void(bool)>&)>;
+      base::OnceCallback<void(const std::string& hostname,
+                              scoped_refptr<net::X509Certificate>,
+                              const base::OnceCallback<void(bool)>&)>;
 
-  void SetVerifyProc(const VerifyProc& proc);
+  void SetVerifyProc(VerifyProc proc);
 
  protected:
   // net::CertVerifier:
   int Verify(const RequestParams& params,
              net::CRLSet* crl_set,
              net::CertVerifyResult* verify_result,
-             const net::CompletionCallback& callback,
+             net::CompletionOnceCallback callback,
              std::unique_ptr<Request>* out_req,
              const net::NetLogWithSource& net_log) override;
   bool SupportsOCSPStapling() override;

atom/browser/net/atom_network_delegate.cc
@@ -307,18 +307,19 @@ void AtomNetworkDelegate::SetDevToolsNetworkEmulationClientId(
 
 int AtomNetworkDelegate::OnBeforeURLRequest(
     net::URLRequest* request,
-    const net::CompletionCallback& callback,
+    net::CompletionOnceCallback callback,
     GURL* new_url) {
   if (!base::ContainsKey(response_listeners_, kOnBeforeRequest))
     return brightray::NetworkDelegate::OnBeforeURLRequest(
-        request, callback, new_url);
+        request, std::move(callback), new_url);
 
-  return HandleResponseEvent(kOnBeforeRequest, request, callback, new_url);
+  return HandleResponseEvent(kOnBeforeRequest, request, std::move(callback),
+                             new_url);
 }
 
 int AtomNetworkDelegate::OnBeforeStartTransaction(
     net::URLRequest* request,
-    const net::CompletionCallback& callback,
+    net::CompletionOnceCallback callback,
     net::HttpRequestHeaders* headers) {
   std::string client_id;
   {
@@ -332,10 +333,10 @@ int AtomNetworkDelegate::OnBeforeStartTransaction(
                        client_id);
   if (!base::ContainsKey(response_listeners_, kOnBeforeSendHeaders))
     return brightray::NetworkDelegate::OnBeforeStartTransaction(
-        request, callback, headers);
+        request, std::move(callback), headers);
 
-  return HandleResponseEvent(
-      kOnBeforeSendHeaders, request, callback, headers, *headers);
+  return HandleResponseEvent(kOnBeforeSendHeaders, request, std::move(callback),
+                             headers, *headers);
 }
 
 void AtomNetworkDelegate::OnStartTransaction(
@@ -351,16 +352,16 @@ void AtomNetworkDelegate::OnStartTransaction(
 
 int AtomNetworkDelegate::OnHeadersReceived(
     net::URLRequest* request,
-    const net::CompletionCallback& callback,
+    net::CompletionOnceCallback callback,
     const net::HttpResponseHeaders* original,
     scoped_refptr<net::HttpResponseHeaders>* override,
     GURL* new_url) {
   if (!base::ContainsKey(response_listeners_, kOnHeadersReceived))
     return brightray::NetworkDelegate::OnHeadersReceived(
-        request, callback, original, override, new_url);
+        request, std::move(callback), original, override, new_url);
 
   return HandleResponseEvent(
-      kOnHeadersReceived, request, callback,
+      kOnHeadersReceived, request, std::move(callback),
       ResponseHeadersContainer(override, original->GetStatusLine(), new_url),
       original);
 }
@@ -436,7 +437,7 @@ template<typename Out, typename... Args>
 int AtomNetworkDelegate::HandleResponseEvent(
     ResponseEvent type,
     net::URLRequest* request,
-    const net::CompletionCallback& callback,
+    net::CompletionOnceCallback callback,
     Out out,
     Args... args) {
   const auto& info = response_listeners_[type];
@@ -447,7 +448,7 @@ int AtomNetworkDelegate::HandleResponseEvent(
   FillDetailsObject(details.get(), request, args...);
 
   // The |request| could be destroyed before the |callback| is called.
-  callbacks_[request->identifier()] = callback;
+  callbacks_[request->identifier()] = std::move(callback);
 
   int frame_tree_node_id = -1;
   GetFrameTreeNodeId(request, &frame_tree_node_id);
@@ -501,7 +502,7 @@ void AtomNetworkDelegate::OnListenerResultInIO(
 
   bool cancel = false;
   response->GetBoolean("cancel", &cancel);
-  callbacks_[id].Run(cancel ? net::ERR_ABORTED : net::OK);
+  std::move(callbacks_[id]).Run(cancel ? net::ERR_ABORTED : net::OK);
 }
 
 template<typename T>

commit d4065164b4ce7120781a644bd29a9ade4fec3864

Fix CHECK failure "FATAL:web_request_api.cc(1935)] Check failed: !blocked_request.callback.is_null()."
caused by
https://github.com/brave/muon/commit/c5188ecf8530b11b7ebea1594dd6ccd875b312c7#diff-588e4771fddbf41e2f95bf26b0ebfcd1R127

STR: enbale dashlane

Auditors: @bridiver, @hferreiro

atom/browser/extensions/atom_extensions_network_delegate.cc
@@ -124,7 +124,7 @@ int AtomExtensionsNetworkDelegate::OnBeforeStartTransaction(
                                 request->identifier());
 
   int result = extensions_delegate_->NotifyBeforeStartTransaction(
-      request, std::move(callback), headers);
+      request, std::move(wrapped_cb), headers);
 
   if (result == net::ERR_IO_PENDING)
     return result;