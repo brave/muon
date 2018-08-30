brave/muon

commit e6bf1d7d209ee796598145381554488efcc16a4e

WIP: Port //services/device/geolocation to network::SimpleURLLoader
https://crrev.com/19aaccdb4552

atom/browser/atom_browser_client.cc
@@ -212,14 +212,6 @@ void AtomBrowserClient::GetAdditionalAllowedSchemesForFileSystem(
                                schemes_list.end());
 }
 
-void AtomBrowserClient::GetGeolocationRequestContext(
-      base::OnceCallback<void(scoped_refptr<net::URLRequestContextGetter>)>
-          callback) {
-  content::BrowserThread::PostTaskAndReplyWithResult(
-      content::BrowserThread::UI, FROM_HERE,
-      base::BindOnce(&GetSystemRequestContextOnUIThread), std::move(callback));
-}
-
 std::string AtomBrowserClient::GetGeolocationApiKey() {
   return GOOGLEAPIS_API_KEY;
 }

atom/browser/atom_browser_client.h
@@ -77,9 +77,6 @@ class AtomBrowserClient : public brightray::BrowserClient,
   void ResourceDispatcherHostCreated() override;
   void GetAdditionalAllowedSchemesForFileSystem(
       std::vector<std::string>* schemes) override;
-  void GetGeolocationRequestContext(
-      base::OnceCallback<void(scoped_refptr<net::URLRequestContextGetter>)>
-          callback) override;
   std::string GetGeolocationApiKey() override;
 
   // brightray::BrowserClient:

commit 26d811a6a9039f9ce4d8df40b7017619541f7961

Fix broken geo location caused by https://github.com/brave/muon/commit/e6bf1d7d209ee796598145381554488efcc16a4e#diff-fb76da0c9cc2defc5c9fa23dd04d5327 https://github.com/brave/muon/pull/642/files#diff-85ff0d997b1a2f089546f7b2823b0ad7L2067
Auditor: @bridiver

atom/browser/atom_browser_client.cc
@@ -25,6 +25,7 @@
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/net/system_network_context_manager.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/renderer_host/pepper/chrome_browser_pepper_host_factory.h"
 #include "chrome/browser/speech/tts_message_filter.h"
@@ -210,6 +211,16 @@ void AtomBrowserClient::GetAdditionalAllowedSchemesForFileSystem(
     additional_schemes->insert(additional_schemes->end(),
                                schemes_list.begin(),
                                schemes_list.end());
+}
+
+scoped_refptr<network::SharedURLLoaderFactory>
+AtomBrowserClient::GetSystemSharedURLLoaderFactory() {
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+  if (!g_browser_process->system_network_context_manager())
+    return nullptr;
+
+  return g_browser_process->system_network_context_manager()
+    ->GetSharedURLLoaderFactory();
 }
 
 std::string AtomBrowserClient::GetGeolocationApiKey() {

atom/browser/atom_browser_client.h
@@ -77,6 +77,8 @@ class AtomBrowserClient : public brightray::BrowserClient,
   void ResourceDispatcherHostCreated() override;
   void GetAdditionalAllowedSchemesForFileSystem(
       std::vector<std::string>* schemes) override;
+  scoped_refptr<network::SharedURLLoaderFactory>
+  GetSystemSharedURLLoaderFactory() override;
   std::string GetGeolocationApiKey() override;
 
   // brightray::BrowserClient:

patches/master_patch.patch
@@ -2599,6 +2599,35 @@ index 7a223958751012b2042a91edf85578b00702d565..08a3940915c61746c7bf80d244008750
    // Called to read raw (pre-filtered) data from this Job. Reads at most
    // |buf_size| bytes into |buf|.
    // Possible return values:
+diff --git a/services/device/geolocation/BUILD.gn b/services/device/geolocation/BUILD.gn
+index 0a139b7b01cb163001db856b32e29e35e450bb1a..48e8580280823008c1f89165a3a9dcbfa6a2458f 100644
+--- a/services/device/geolocation/BUILD.gn
++++ b/services/device/geolocation/BUILD.gn
+@@ -14,6 +14,7 @@ source_set("geolocation") {
+     "//services/device:*",
+     ":*",
+   ]
++  configs += [ "//electron/build/geolocation" ]
+ 
+   sources = [
+     "empty_wifi_data_provider.cc",
+diff --git a/services/device/geolocation/network_location_request.cc b/services/device/geolocation/network_location_request.cc
+index c5cd5bd74f1228cce0449b0a20a6a30132c7851d..f3c601b17e9c0232481c604ca96aeda38047a0e0 100644
+--- a/services/device/geolocation/network_location_request.cc
++++ b/services/device/geolocation/network_location_request.cc
+@@ -31,8 +31,12 @@
+ namespace device {
+ namespace {
+ 
++#if defined(GOOGLEAPIS_ENDPOINT)
++const char kNetworkLocationBaseUrl[] = GOOGLEAPIS_ENDPOINT;
++#else
+ const char kNetworkLocationBaseUrl[] =
+     "https://www.googleapis.com/geolocation/v1/geolocate";
++#endif
+ 
+ const char kLocationString[] = "location";
+ const char kLatitudeString[] = "lat";
 diff --git a/services/service_manager/sandbox/win/sandbox_win.cc b/services/service_manager/sandbox/win/sandbox_win.cc
 index 7beb41460e7d1a7340cca8ebc98a114ec41b3e44..3b3e1aa30f342971b96b4213ed85718d36fb3aa6 100644
 --- a/services/service_manager/sandbox/win/sandbox_win.cc